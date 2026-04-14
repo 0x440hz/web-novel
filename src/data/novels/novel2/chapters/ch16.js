9008d05cfd231cb4668856e89c6c7edc3f47cb2d
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 06:33:38 +0700

    Add chapter 16: Sumpah Dendam di Balik Luka

---
+++ src/data/novels/novel2/chapters/ch16.js
@@ -0,0 +1,11 @@
+const chapter = {
+  id: 16,
+  arc: "Arc 2: Flame Arc",
+  title: "#16 Sumpah Dendam di Balik Luka",
+  content: `
+---------------------------------
+
+---------------------------------`,
+};
+
+export default chapter;
