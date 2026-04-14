cb754322c574898ad5827e59ad3dda4def9f1753
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 21:09:13 +0700

    Update ch8.js

--- src/data/novels/novel2/chapters/ch8.js
+++ src/data/novels/novel2/chapters/ch8.js
@@ -1,10 +1,67 @@
 const chapter = {
-  id: 5,
+  id: 8,
   arc: "Arc 2: Flame Arc",
-  title: "#5 Sumpah Dendam di Balik Luka",
+  title: "#8 Saat Waktu Berhenti untuk Dua Jiwa",
   content: `
 ---------------------------------
+Kryle menuntun Reina untuk Duduk sebentar di salah satu bangku yang ada disana. 
 
+Kryle:"Tunggu disini Reina aku akan mengambilkan air untuk kau minum Reina"
+Reina:"Tidak perlu Kryle, Beritahukan saja pada semua orang jika air sudah bersih"
+Kryle:".... Um.. Baiklah, tapi tetap saja aku akan segera kembali!"
+
+Kryle pergi menjauh dari Reina dan melakukan yang harus dilakukan sementara Reina tetap terduduk sendiri di bangku itu. 
+---------------------------------
+_"Chrona, sistea-tempus sta."_
+*Tiba-tiba saja waktu terasa terhenti, dan semua orang disana juga ikut berhenti seakan membeku secara tiba-tiba, hanya saja Reina Sadar bahwa hanya dirinya sendiri yang tetap bergerak*
+
+Reina:"!?! "
+Zavyre:"Tenang saja ini tidak akan lama"
+
+*Tiba-tiba saja sosok Zavyre sudah duduk di samping Reina.*
+
+Reina:".. Kau, kenapa kau kembali?!"
+Zavyre:"Aku sudah bisa mengukur kekuatan mu hanya dari cara mu memurnikan danau itu... Dan ternyata yang di katakan para penyihir dulu itu benar, kau Kuat. "
+Reina:"apa tujuan mu kesini sebenarnya?"
+Zavyre:"Hanya satu.. Reina"
+
+*Tiba-tiba saja dia bergerak bagaikan angin kedepan wajah Reina dan mengangkat Dagu Reina dengan tangannya sehingga mereka saling bertatapan dengan jarak yang sangat dekat*
+
+Zavyre:"Satu hal Reina-ku... Membalas dendam Ayahku padamu"
+Reina:"kau-!..."
+
+Aura yang ia pancarkan dingin dan menekan, memberikan kesan bahwa satu kata saja darinya bisa menjadi ancaman. Namun di balik sikap mengintimidasi itu, ada getaran halus yang tak terlihat jemarinya kerap menegang tanpa sadar, aku bisa melihatnya dari sisi lain di matanya. Perasaan itu terasa asing bagi kepribadiannya.. Takut?Cemas?malu? 
+---------------------------------
+*Seketika waktu kembali berputar dan semua orang kembali bergerak, dan bersamaan saat itu juga Zavyre sudah pergi*
+
+Reina:"......."
+Aku hanya mematung di situ,memikirkan apa yang baru saja terjadi.
+
+*Beberapa menit kemudian Kryle datang kembali pada Reina dengan membawakan air untuk Reina.*
+
+Kryle:"Reina.. Ini minumlah"
+Reina:"............ "
+Kryle:"hey, Reina.."
+Reina:"Ah?!?.. Oh! Kau sudah kembali-"
+Kryle:"apa yang sedang kau pikirkan Reina? Akhir-akhir ini kau banyak melamun.."
+Reina:"aku bingung Kryle..."
+Kryle:"soal apa?"
+Reina:"Kryle! Bagaimana jika dirimu adalah seorang yang dingin, mengintimidasi, dan kaku.. Tetapi kau menyukai seseorang?!!!"
+
+Wajah Kryle memerah dengan pertanyaan ambigu yang tiba-tiba saja keluar dari mulut Reina. 
+
+Kryle:"H.. Huhhh?Apa maksud mu Reina? "
+Reina:".. Ah-.. ahhhhh.. Maksud ku... AHH LUPAKAN SAJA"
+
+Untuk beberapa detik kemudian Aku melihat wajah kryle yang masih me-merah, dan kita juga terdiam untuk sesaat, sampai akhirnya kryle memecah keheningan antara kita berdua. 
+
+Kryle:"K-kalo begitu Reina ayo.. Kita kembali ke istana.. Mungkin kau juga sudah terlalu kelelahan"
+Reina:"Mungkin kau ada benarnya juga.. Baiklah...ayo kita pulang..kryle"
+Kryle:"Hehe.. Ayo Yang Mulai"
+
+Aku meraih tangan kryle, dan wajah merah kryle terlihat semakin memerah saat aku meraih tangannya
+
+_"Celeris"_.
 ---------------------------------`,
 };
 
