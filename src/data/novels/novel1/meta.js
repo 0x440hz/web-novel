e5beee70ebc7a937864770ce8e458f28e20fd081
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 16:04:08 +0700

    Update meta.js

--- src/data/novels/novel1/meta.js
+++ src/data/novels/novel1/meta.js
@@ -1,7 +1,7 @@
 const meta = {
   id: "novel1",
   slug: "novel1",
-  title: "Even the Stars Bowed to Her",
+  title: "Even the Stars Bowed to Her (Moon Arc)",
   synopsis:
     "Di dunia yang berubah saat malam tiba, iblis mengancam kehidupan manusia, dan para pemburu menjadi satu-satunya garis pertahanan. Rathail, seorang pemimpin yang dingin dan tegas, memimpin timnya menghadapi serangan demi serangan yang semakin brutal. Namun di balik pertarungan melawan iblis, mereka juga dihadapkan pada konflik batin, kepercayaan, dan hubungan yang perlahan mengubah mereka. Di bawah cahaya bulan, kisah ini bukan hanya tentang bertahan hidup, tetapi tentang menemukan arti kekuatan, pengorbanan, dan kemanusiaan di tengah kegelapan.",
   author: "Rerei",
