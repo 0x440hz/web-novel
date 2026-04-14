e3e23fcb584f559a45fbdd2b86498255e1b30026
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 20:50:20 +0700

    Update ch4.js

--- src/data/novels/novel2/chapters/ch4.js
+++ src/data/novels/novel2/chapters/ch4.js
@@ -1,1 +1,62 @@
+const chapter = {
+  id: 4,
+  arc: "Arc 2: Flame Arc",
+  title: "#4 Kebangkitan yang Terlupakan",
+  content: `
+---------------------------------
+*Vulkrath adalah kerajaan yang cukup luas dengan penduduknya yang saling bisa diandalkan satu sama lain, semuanya hidup dengan gembira seakan tidak ada yang perlu dikhawatirkan sama sekali.. Mengesampingkan fakta bahwa Vulkrath sebelumnya pernah di Hancurkan*
+---------------------------------
+Setelah sedikit perdebatan dengan kryle akhirnya dia setuju dan membiarkan ku bisa pergi ke desa sendirian.
+
+Reina:"Syukurlah keadaan di desa baik-baik saja"
+
+Aku pun melanjutkan perjalanan di Desa dan melihat banyak sekali aktivitas para Rakyat Vulkrath yang begitu sederhana tetapi tampak penuh makna.
+Pengamanan di setiap sudut desa pun sudah cukup beberapa prajurit yang berjaga sempat menyapaku beberapa kali. 
+---------------------------------
+Reina:".......... "
+
+Tiba-tiba saja aku merasakan sesuatu atau mungkin..seseorang sedang memperhatikan ku dibalik riuh orang banyak,aku berusaha melihat ke sekeliling..
+
+Di suatu titik aku sekilas melihat seseorang dengan jubah berwarna hitam yang pergi ke suatu sudut desa. 
+
+Dan tiba-tiba saja ada dorongan yang memaksaku untuk mengikuti figur misterius itu ke sudut-sudut desa yang bahkan aku tidak tau bahwa tempat itu ada di desa. 
+---------------------------------
+Reina:"...."
+
+Ntah mengapa Tiba-tiba saja hawa di tempat itu berubah, semuanya meredup, cahaya siang hari yang menyilaukan perlahan tenggelam dalam gelap, suara riuh penduduk desa perlahan membisu..... 
+
+Semua seakan Hanya ada aku disini,di tempat yang sunyi dan terasa kosong.. 
+---------------------------------
+?? :"mengapa kau mengikutiku kemari,Reina....."
+Reina:"... Aku ingin mengetahui siapa dirimu dan apa tujuanmu sebenarnya datang ke kerajaan ku.. "
+---------------------------------
+Perlahan dia membuka jubahnya yang menutupi wajahnya. 
+---------------------------------
+Tiba-tiba rasanya ada kekuatan sihir yang terasa tidak asing mulai menyelimuti tempat itu.
+
+??:"..apa kau merasa familiar dengan kekuatan ini?"
+?? :"karna memang seharusnya kau mengenali kekuatan sihir ini.. Bahkan setelah belasan tahun"
+Reina:"..... Apa maksudmu?! Siapa dirimu sebenarnya!?!"
+Zavyre:"Reina...Putri kesayangan warga Vulkrath, tidaklah kau melupakan diriku?...Aku Zavyre. 
+Reina:"Zavyre?..... "
+Zavyre:"benar aku adalah salah satu dari Penyihir yang kau kalahkan 18 tahun lalu. 
+Reina:".... Kau.. Salah satu dari mereka........ "
+Zavyre:"tch.. Mengingat kekalahan itu saja membuatku sangat jijik.. Semua penyihir itu memang payah.. Mereka bahkan tidak bisa mengalahkan seseorang Putri Raja yang lugu Seperti dirimu Reina.... "
+Zavyre:"bodohnya aku percaya pada perkataan salah satu Penyihir itu, yang mengatakan kau bukan hanya putri Raja biasa.. "
+Reina:".. Mengalahkan penyihir 18 tahun lalu?.... Aku??!!.. Aku tidak mengerti dengan perkataan mu itu-"
+
+Tiba-tiba saja energi yang cukup besar terasa menghantam diriku hingga tubuhku terlempar mundur.
+
+Zavyre:"JANGAN PURA-PURA BODOH REINA!"
+Zavyre:"Aku benar tau, jika kau memiliki kekuatan yang sangat besar, hingga bisa membuat penyihir yang kuat itu mati dalam sekaligus. Maka dari itu aku akan membalaskan dendam para penyihir itu padamu.. DimulaiDari detik ini REINA!!!"
+---------------------------------
+Dia sepertinya mulai melemparkan serangan pertamanya. 
+
+_(Reina:"Ayo lakukan demi Melindungi Vulkrath, Hartaku satu-satunya yang tersisa")_
+...
+_"Ferrum Lucis-Secta!"_
+---------------------------------`,
+};
+
+export default chapter;
 
