1483359f64cfe9f1dc24782b94e178edeebe5d95
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 20:53:06 +0700

    Update ch5.js

--- src/data/novels/novel2/chapters/ch5.js
+++ src/data/novels/novel2/chapters/ch5.js
@@ -1,1 +1,106 @@
+const chapter = {
+  id: 5,
+  arc: "Arc 2: Flame Arc",
+  title: "#5 Sumpah Dendam di Balik Luka",
+  content: `
+---------------------------------
+"FERRUM LUCIS-SECTA!"
 
+Zavyre:"!!!.. "
+
+*Ratusan Bilah-bilah tajam yang terbuat dari cahaya mengenainya dan langsung menembus tubuhnya yang hendak melancarkan serangan.*
+
+Reina:" * sigh * hah..hah....untungnya.. Di tempat ini masih memiliki sumber cahaya.. Walau tidak terlalu banyak.. "
+
+(Reina:"ruangan hampa ini.. Pasti buatannya.. Untuk Sementara aku harus mengulur kekuatannya terlebih dahulu agar aku bisa mengetahui kelemahan dan semua jenis serangan nya.. ") 
+
+Zavyre:"Heh..!! Serangan mu tadi itu sia-sia!!"
+(Reina:"dia-..masih bisa bertahan?!?!") 
+Zavyre:"Vis Ruptura!!"
+
+*secara tiba-tiba dia mendekat kearah Reina yang nampak lengah dengan gerakan yang sangat cepat hingga wajahnya berjarak sangat dekat dengan Reina*
+
+Reina:_"Celeris!"_ hah..untung nya Aku berhasil berpindah tempat tepat waktu... 
+Reina:"serangan-mu tadi itu..-!!"
+Zavyre:"TIDAK ADA WAKTU UNTUK BERPIKIR REINA!!!!"
+Zavyre:_"Vis Ruptura-Detonare_!!!! 
+
+*Dia kembali bergerak ke depan  Reina dan kali ini serangan nya mengenai Reina tepat di tubuhnya, hingga dia terpental ke belakang dengan cukup keras*
+
+Reina:"!!!!.."
+
+*Setelah dia berhasil mengenai Reina ruangan hampa tadi memudar lalu mereka kembali ke pemandangan desa tempat mereka semula*
+
+*Melihat Reina yang terkapar dia berjalan mendekati nya lalu berlutut di samping nya sembari mengangkat beberapa helai rambut Reina yang sedang terbaring Lemah*
+
+Zavyre:"Rambut lembut yang bagus putri Reina, tapi kau akan segera kehilangan rambut indah mu ini... "
+
+Reina:_"Solaris Bastille "_
+Zavyre:"!!!!!!"
+
+*Tiba-tiba kurungan panas yang terbuat dari cahaya matahari mengurung Zavyre yang tidak sempat menghindar*
+
+Zavyre:"APA YANG KAU-!! KEPARAT!!"
+Zavyre:_"Detonare"_!! AHHKK!!!"
+
+Perlahan Reina bangkit dari posisi sebelumnya. 
+
+Reina:"Percuma saja.. Setiap serangan mu akan berbalik pada dirimu sendiri jika kau berada di dalam kurungan itu"
+
+*Terlihat bahwa wajah Zavyre mulai me-merah karna frustasi.*
+
+Zavyre:"Detonare!!!".. AKHHGG!! BAJINGAN!! 
+Reina:"Hentikan! Kau hanya akan membunuh dirimu sendiri"
+Zavyre:"tch..!! Aku belum selesai denganmu Reina! _"Exodus"_
+
+Aku Menghalangi wajahku dengan lengan hingga saat aku melihat kembali ke sekeliling ku Zavyre sudah menghilang ntah kemana bersama kurungan yang aku buat sebelumnya. 
+---------------------------------
+Reina:"... Dia pergi"
+
+Tak lama suara langkah yang ku kenal datang mendekat disertai suara yang sangat tidak asing bagi ku. 
+
+Kryle:".. REINA! Disitu kau rupanya!...maksud ku..putri.."
+Reina:"hehe.. Ada apa kryle?"
+Kryle:"Tiba-tiba saja aku merasakan kekuatan sihir mu dari istana.. Itu mengapa aku langsung pergi untuk mencari mu.. Apa yang kau lakukan di gang sepi seperti ini?! Apa kau baru saja bertarung?!?"
+Reina:"ah.. Tidak².. Aku hanya melatih sihir ku.. Itu saja.."
+Kryle:"... Benarkah? "
+Reina:"tentu saja.. Lagi pula pastinya kekuatan sihir ku yang kau rasakan tidak besar kan?mungkin sangat sedikit"
+Kryle:".. B-benar juga.. Tapi tetap saja aku agak khawatir, karna kau jarang menggunakan kekuatan sihir mu.."
+Reina:"sudahlah kryle aku tidak apa-apa.. Kau hanya terlalu Khawatir "
+
+Kryle hanya mengangguk tetapi tetap terpancar keraguan pada wajahnya.
+Aku pun meraih wajahnya dengan kedua tanganku dan mengusapnya dengan lembut, dan senyuman diwajahku.
+ 
+Reina:"Ayo kita kembali kryle.."
+Kryle:"baiklah.... "
+
+Diperjalanan kembali ke istana aku tetap memikirkan Zavyre dan khawatir jika sewaktu-waktu dia akan kembali kesini.
+
+_("Bagaimana jika semua kebahagiaan ku ini akan direnggut lagi olehnya?")_
+---------------------------------
+*Di sisi lain Zavyre telah berhasil mengeluarkan dirinya sendiri dari kurungan buatan Reina dengan tubuh penuh luka karna kekuatan yang terus kembali menyerang dirinya*
+
+Zavyre:"... Hah..... Hah....wanita itu..Dasar Keparat!"
+Zavyre:"aku bersumpah aku akan menghancurkan semua yang dia sayangi!!"
+
+*Rasa sakit yang luar biasa masih bisa dia rasakan dan sihir penyembuhan nya saja belum cukup untuk menyembuhkan nya dengan cepat*
+
+Zavyre:"...hah.."
+*zavyre mengusap keningnya dengan lengan dan mendapati beberapa helai rambut Reina yang terbawa oleh nya*
+
+Zavyre:"tch.."
+
+*Dia mengambil rambut itu dan hendak membuangnya ke tanah tapi sebelum dia sempat melemparkan rambut itu ke tanah sesaat dia teringat perkataan Reina.*
+
+_"Hentikan, itu hanya akan membunuh dirimu sendiri"_
+
+Zavyre:"........ Kenapa dia menyuruhku berhenti... -"
+Zavyre:"TUNGGU APA YANG AKU PIKIRKAN!?! "
+
+*Akhirnya dia melanjutkan untuk membuang rambut itu ke tanah*
+
+*_Zavyre:".. Aku akan menghancurkan mu Reina.. Dan itu Pasti!!!!"_*
+---------------------------------`,
+};
+
+export default chapter;
