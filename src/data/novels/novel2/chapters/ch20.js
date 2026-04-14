f60150cfecd123ca866c1161a7da01e438586123
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 06:40:30 +0700

    Add chapter 20 to novel2

---
+++ src/data/novels/novel2/chapters/ch20.js
@@ -0,0 +1,11 @@
+const chapter = {
+  id: 20,
+  arc: "Arc 2: Flame Arc",
+  title: "#20 Sumpah Dendam di Balik Luka",
+  content: `
+---------------------------------
+
+---------------------------------`,
+};
+
+export default chapter;
