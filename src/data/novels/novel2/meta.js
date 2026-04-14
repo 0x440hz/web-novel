f67f0a66c2e70c878ef9e18672c55dbbe1a9148b
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 16:13:46 +0700

    Update meta.js

--- src/data/novels/novel2/meta.js
+++ src/data/novels/novel2/meta.js
@@ -1,10 +1,11 @@
 const meta = {
   id: "novel2",
   slug: "novel2",
-  title: "Ini placeholder, nanti ganti sendiri",
-  synopsis: "Ini placeholder, nanti ganti sendiri",
+  title: "Even the Star Bowed to Her (Flame Arc) ",
+  synopsis: "Reina adalah seorang gadis yang secara misterius mampu mengingat momen saat dirinya baru lahir,kehangatan suara orang tuanya, harapan, dan cinta yang menyertai namanya, seolah ia ditakdirkan untuk bersinar seperti bintang. Namun, kenangan itu berbanding terbalik dengan kenyataan yang ia hadapi saat terbangun di tengah dunia yang hancur oleh api, jeritan, dan peperangan tanpa ampun. Dalam kebingungan dan ketakutan, sebuah suara bernama Kryle memanggilnya untuk bangkit dan menghadapi ancaman para penyihir yang membawa kehancuran. Tanpa memahami siapa dirinya sebenarnya atau mengapa semua ini terjadi, Reina dipaksa melangkah maju di tengah kekacauan, menjadikan masa lalunya sebagai satu-satunya petunjuk menuju kebenaran, dalam perjalanan yang perlahan mengungkap takdir besar dan kekuatan tersembunyi dalam dirinya.
+",
   author: "Rerei",
-  status: "Ongoing",
+  status: "Completed",
   cover: null,
 };
 
