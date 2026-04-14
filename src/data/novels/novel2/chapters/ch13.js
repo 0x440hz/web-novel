2d6603ac4433732f96d1fcc7125230d3b1995e1c
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 06:19:29 +0700

    Add chapter 10 to novel2

---
+++ src/data/novels/novel2/chapters/ch13.js
@@ -0,0 +1,11 @@
+const chapter = {
+  id: 10,
+  arc: "Arc 2: Flame Arc",
+  title: "#10 Api yang Lahir dari Kehilangan",
+  content: `
+---------------------------------
+
+`,
+};
+
+export default chapter;
