4471502b28d7611b4cf3d59b7f980fd9f5f0f776
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 21:19:44 +0700

    Update and rename src/data/novels/novel2/characters/karakter-a.js to src/data/novels/novel2/Reina.js

---
+++ src/data/novels/novel2/Reina.js
@@ -0,0 +1,19 @@
+export default `
+*Biodata Karakter*
+
+Nama:Reina
+Gender:Perempuan
+Peran:Tokoh utama / pewaris cahaya / pusat konflik magis
+*Kepribadian:*
+  Penuh rasa ingin tahu dan cerdas
+  Penuh empati namun tegas ketika menghadapi bahaya
+  * Memiliki aura misterius dan menawan, membuat orang terkesan sekaligus menghormatinya
+*Kemampuan / Kekuatan:*
+Sihir cahaya, mampu membentuk serangan, perisai, atau kurungan cahaya
+Memiliki kekuatan destruktif sekaligus protektif
+Bisa memadukan cahaya menjadi bilah tajam atau kurungan panas (*Solaris Bastille*)
+
+*Gaya Bertarung:*
+Kombinasi **serangan jarak jauh dan pertahanan**, memanfaatkan cahaya untuk menaklukkan musuh
+Fleksibel: bisa menyerang, menahan, atau melindungi sekutu.`
+  ;
