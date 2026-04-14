6cd331c2b0888777605b0ceb4756a7ad94ef46d2
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 20:47:29 +0700

    Update ch3.js

--- src/data/novels/novel2/chapters/ch3.js
+++ src/data/novels/novel2/chapters/ch3.js
@@ -1,1 +1,82 @@
+const chapter = {
+  id: 3,
+  arc: "Arc 2: Flame Arc",
+  title: "#3 Awal dari Pertemuan Takdir",
+  content: `
+---------------------------------
+*TimeSkip*
+
+*18 Tahun setelah peperangan yang menjadikan tanah kerajaan yang subur menjadi terbakar dalam keabadian, semuanya sudah berubah.*
+
+*Vulkrath.. Begitulah orang-orang sekarang menyebut Tanah yang terbakar ini*
+---------------------------------
+Kryle:"Reina..."
+Reina:"mm... "
+Kryle:"Reina buka matamu, sudah saatnya kau memberikan sedikit kata-kata motivasimu pada semua orang di kerajaan, Kau ingat?"
+Reina:"tch.. Baik-baik.. Aku bangun.. Kau ini..pidato itu bisa ku lakukan dalam beberapa menit lagi"
+Kryle:"Dasar pemala-.. "
+Reina:"Apa katamu?! "
+Kryle:"bukan apa-apa....... AYO AYO REINAAA"
+---------------------------------
+18 tahun sejak itu, aku di putuskan untuk menjadi penerus tahkta kerjaan Vulkrath ini, banyak yang harus dilakukan seorang pemimpin di kerajaan yang lumayan luas ini, sebenarnya aku cukup senang menjadi pemimpin..tetapi selalu terbesit dalam pikiran ku...._"kenapa aku?"_...
+
+Akhirnya beberapa menit kemudian aku mengumpulkan keberanian untuk membuka pintu istana dan melihat semua wajah rakyat di kerajaan Vulkrath
+
+Pintu istana yang tinggi nan megah perlahan mulai terbuka, dan sedikit demi sedikit aku bisa melihat setiap mata rakyat Vulkrath yang penuh dengan harapan dan sukacita. 
+
+Setelah melihat pandangan penuh harapan itu, Untuk pertama kalinya aku berkata pada diriku sendiri _"Aku harap bisa mempertahankan perasaan penuh suka cita ini"_
+---------------------------------
+*Suara para rakyat kerajaan yang bersorak-sorai ketika Figur Reina muncul dihadapan mereka semua*
+
+"Putri Reina!!"
+"Reinaaa!putri Vulkrath"
+"Semuanya akan baik-baik saja jika kau ada bersama kami putri!!"
+
+*Mendengarkan para rakyat yang bersorak-sorak kryle maju kedepan Reina dan mengisyaratkan para rakyat untuk senyap, seketika para rakyat mulai merendahkan suara mereka masing-masing dan setelah semua suara berhenti Kryle mengisyaratkan Reina untuk maju.*
+---------------------------------
+Reina:"Wahai rakyatku yang kucintai,
+hari ini aku berdiri di hadapan kalian bukan hanya sebagai putri kerajaan, tetapi sebagai bagian dari kalian semua. Kekuatan sebuah kerajaan tidak lahir dari mahkota, melainkan dari persatuan dan kepercayaan antara pemimpin dan rakyatnya."
+
+"Aku berjanji akan menjaga negeri ini dengan hati yang jujur, keputusan yang adil, dan keberanian untuk melindungi yang lemah. Namun ketahuilah, kejayaan kerajaan ini tidak dapat berdiri tanpa peran dan doa dari kalian semua."
+
+"Mari kita melangkah bersama, saling menguatkan, dan membangun masa depan yang damai serta bermartabat. Selama kita bersatu, tak ada badai yang mampu meruntuhkan kerajaan kita."
+---------------------------------
+setelah pidato yang singkat itu suara sorak-sorak rakyat kembali menggema di sekeliling ku. 
+Aku melihat satu persatu wajah rakyat ku yang penuh harapan itu, namun ntah mengapa mataku terpaku pada satu orang yang nampak asing diantara para rakyat kerajaan yang ada disana. 
+Saat Mata kita saling bertemu ntah mengapa pikiran ku seakan meminta ku untuk menghampiri nya.ketika aku masih terpaku pada sosok misterius itu Kryle tiba-tiba memecahkan pandangan ku yang terpaku karena Kryle tiba-tiba berdiri didepanku dan seakan melihat hal yang sama denganku dia mengisyaratkan ku untuk masuk kedalam istana dan para rakyat pun hendak kembali pada aktivitas mereka masing-masing. 
+---------------------------------
+Kryle:"Reina... Masuklah sudah selesai, aku akan mengurus semuanya"
+
+Aku hanya mengangguk dan hendak masuk ke dalam istana juga namun langkah ku terhenti sejenak dan membalikkan pandangan ku pada kryle. 
+
+Reina:"kryle..tidak perlu sampai bertindak"
+
+Setelah berkata seperti itu kryle langsung memasang wajah tegang dan khawatir
+
+Kryle:"tapi Reina... Kenapa?.. Aku merasakan ada sesuatu yang aneh dari orang itu "
+
+Reina:"Itu mungkin hanya perasaan mu, jangan terlalu dipikirkan.. Mungkin dia hanya seorang pengembara biasa"
+
+Kryle:"baiklah.. Jika itu adalah permintaan mu.. "
+
+Reina:"terimakasih... Omong-omong aku berniat untuk pergi keluar sendirian hari ini"
+
+Kryle:"ku tolak!Kau tidak boleh pergi sendirian keluar walaupun hanya untuk bertemu dengan rakyat mu di desa"
+
+Reina:"mengapa begitu kryle?Aku akan baik-baik saja..lagipula di setiap penjuru kerajaan ini tersebar prajurit yang berjaga bukan? 
+
+Kryle:"... Setidaknya biarkan aku ikut dengan mu, Reina"
+
+Reina:"Ku Tolak!"
+
+Kryle:"..... Yang benar saja"
+
+Diriku tersenyum karna melihat wajah yang sedikit frustasi pada kryle. 
+Sosok misterius itu tetap berada dalam pikiran ku, dan pikiran ku mengatakan memang ada sesuatu yang berhubungan dengannya, itulah alasanku yang lain untuk pergi keluar sendirian. 
+
+_"Aku harap bukan sesuatu yang buruk"_
+---------------------------------`,
+};
+
+export default chapter;
 
