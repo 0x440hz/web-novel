28ed36f959d484462de15416c8ca0c9fffbeb8e7
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 18:33:45 +0700

    Update and rename ch2 to ch2.js

---
+++ src/data/novels/novel2/chapters/ch2.js
@@ -0,0 +1,40 @@
+const chapter = {
+  id: 1,
+  arc: "Arc 2: Flame Arc",
+  title: "#2 Nyala Terakhir Sebuah Kerajaan",
+  content: `
+---------------------------------
+Kryle:"REINA!!!, BANGUN!!"
+
+Saat penglihatan ku kembali, aku bisa melihat dengan jelas tatapan yang penuh ketakutan, kekhawatiran, dan nyari putus asa dalam mata Kryle. 
+
+Reina:"Kryle... " 
+'Sahut ku pada suara Kryle yang terdengar bergetar saat memanggil namaku'
+
+Kryle:"Reina.. Para penyihir itu mencoba untuk menghancurkan kerajaan ini.. Apa yang harus kita lakukan Reina?!?!.. 
+---------------------------------
+Kesadaran ku memang belum pulih sepenuhnya dan penglihatan ku juga mulai kembali kabur tapi aku tetap memaksakan tubuh ku untuk berdiri
+
+Sendi-sendi ku terasa sangat kaku, setiap bagian tubuhku rasanya mati, yang aku rasakan saat itu hanyalah rasa sakit yang nyaris seperti tidak ada akhirnya.
+
+Aku berusaha melihat ke sekeliling ku dan mendapati dua tubuh orang yang tidak asing bagiku, _Tiara ungu_ disalah satu tubuh itu....Tidak salah lagi,Raga yang nampak kosong itu adalah raga orang tuaku yang ntah bagaimana keadaannya... Sebelum aku bisa berteriak dan mengalirkan air mata yang seharusnya dilakukan seseorang ketika kehilangan sesuatu yang sangat berharga baginya, saat itu yang terakhir kali aku ingat adalah suara Kryle yang mulai merintih dan terus berkata "ini adalah akhir" yang di iringi dengan cahaya merah menyala yang perlahan melahap semua yang ada di depan pandangan ku. 
+
+Tiba-tiba semuanya menjadi sangat terang dan dalam sekejap pula-............. 
+
+... 
+... 
+...
+
+*Semuanya Gelap*
+
+_Reina:"aku harap aku bisa terbangun di tempat yang damai"_
+---------------------------------
+*Setelah kejadian itu, banyak korban yang berjatuhan, banyak jiwa yang tidak bersalah juga ikut direnggut dari kerjaan.*
+
+*Anehnya Walaupun sudah lebih dari 18 tahun sejak 'Peperangan' itu tanah di kerajaan tetap terlihat seperti Terbakar dan membara seakan kerajaan itu terbakar dalam keabadian.*
+
+*Di keadaan ini orang-orang mulai beradaptasi dengan tanah yang terus menerus terbakar sampai orang-orang mulai menyebut kerjaan itu _"Vulkrath"_*
+---------------------------------`,
+};
+
+export default chapter;
