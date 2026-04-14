c5c35fe67491576b2c56ed060fa7df2bb65a99d6
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 21:12:02 +0700

    Update ch9.js

--- src/data/novels/novel2/chapters/ch9.js
+++ src/data/novels/novel2/chapters/ch9.js
@@ -1,10 +1,54 @@
 const chapter = {
-  id: 5,
+  id: 9,
   arc: "Arc 2: Flame Arc",
-  title: "#5 Sumpah Dendam di Balik Luka",
+  title: "#9 Sihir yang Mengkhianati Hati",
   content: `
 ---------------------------------
+*Derap langkah dari seseorang, yang terdengar sangat berat datang memasuki gua yang tersembunyi jauh dari tanah Vulkrath. Udara di sana terasa berat dan dingin, bukan karena suhu, melainkan karena sisa-sisa mantra lama yang masih menggantung, tak pernah benar-benar menghilang. Setiap tarikan napas membawa aroma batu basah, abu, dan logam tua bau khas sihir yang telah terlalu sering dipaksakan melampaui batas alam,ditempat itulah Zavyre menetap, jauh dari Vulkrath,jauh dari peradaban,dan jauh dari siapapun yang bisa membuat keraguannya muncul*
 
+Zavyre:"..."
+???:"OHH!! ZAVYRE KAU SUDAH PULANG!
+Zavyre:"apa tujuanmu berteriak seperti itu kepadaku, Vyerna?. 
+Vyerna:"pssss.. Aku merindukan mu kau sudah pergi sangat lama Zavyre"
+Zavyre:"tch.. Dasar wanita ular"
+Vyerna:"Psss-.. Tidak sopan! Aku ini memang ular!"
+Vyerna:"Jadi Zavyre~..apa kau berhasil menghabisi Putri Raja ituu~"
+Zavyre:"Belum"
+Vyerna:"psss!! Apa kau takut padanya~"
+Zavyre:"TENTU SAJA TIDAK!"
+Vyerna:"Tidak? ~.. Tapi Zavyre sayangku, tangan mu bergetar, dan tatapan mata mu tidak seperti biasanya"
+
+*Zavyre merasakan tangan vyerna merayap di sekujur tubuh nya, seakan sedang mencoba untuk menggoda perasaan Zavyre*
+
+Zavyre:"hentikan itu.. Aku hanya perlu waktu yang tepat untuk menghabisi nya"
+Vyerna:"tapi Zavyre, apa kau yakin akan menghabisinya dengan tatapan penuh perasaan seperti itu?..tatapan Itu sangat asing bagimu, ada sesuatu yang menghambat mu Zavyre~
+Zavyre:"apa maksud mu?.. Apa yang bisa menghambat ku?"
+Vyerna:"Perasaan mu"
+Zavyre:"..... Tch apa maksudnya itu, tentu saja aku tidak akan terpengaruh oleh hal bodoh seperti itu "
+Vyerna:"ada perasaan asing dalam dirimu Zavyre"
+Zavyre:"perasaan apa yang kau maksud?!"
+Vyerna:"psss~... Aku Merasakan~.. Ada sedikit rasa cinta dalam dirimuu. 
+Zavyre:"omong kosong!,kau hanya mengada-ada!"
+Vyerna:"Ahh.. Aku lupa kau orang yang seperti itu"
+Zavyre:"aku akan menghancurkan Reina itu tujuanku tidak ada yang lain!"
+Vyerna:".. Tapi aku tidak mengatakan soal Reina~"
+
+*mata Vyerna melirik dengan penuh kecurigaan yang jahil*
+
+Vyerna:"pss~ aku mengerti"
+Zavyre:"DASAR WANITA ULAR!! "
+Zavyre:"DARC BASTILLE!!"
+
+*Tiba-tiba ada kurungan yang mengurung Vyerna ditempatnya*
+
+Vyerna:"HUH?!!, HEY!! KELUARKAN AKU!!"
+Vyerna:"...... Tapi Omong-omong kau mempelajari sihir ini dari salah satu sihir Reina kan? "
+
+*tatapan jahil Vyerna kembali di wajahnya*
+
+Zavyre:"TCH.. Tak usah ikut campur"
+Vyerna:"psss.. Tentu saja itu sangat mencerminkan dirimu"
+Zavyre:"diam lah... Lihat saja aku akan menjadi semakin kuat dan menghabisi Reina. 
 ---------------------------------`,
 };
 
