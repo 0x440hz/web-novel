27ae6e4840a453a4b14ee88ccfd59445ad79c040
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 21:06:47 +0700

    Update ch6.js

--- src/data/novels/novel2/chapters/ch6.js
+++ src/data/novels/novel2/chapters/ch6.js
@@ -1,10 +1,72 @@
 const chapter = {
-  id: 5,
+  id: 6,
   arc: "Arc 2: Flame Arc",
-  title: "#5 Sumpah Dendam di Balik Luka",
+  title: "#6 Benang Takdir yang Terhubung",
   content: `
 ---------------------------------
+Beberapa hari Setelah pertarungan singkat diriku dengan Zavyre untungnya tidak ada yang berubah dari Vulkrath tidak sedikit pun dan aku hanya berharap tidak akan pernah terjadi sesuatu yang buruk. 
 
+Untuk ke sekian kalinya setelah itu aku selalu menghabiskan waktu senggang di perpustakaan istana mencari tahu petunjuk tentang orang itu, dan seperti yang aku duga, aku sama sekali tidak menemukan apapun tentangnya selain para penyihir yang menyerang 18 tahun lalu. 
+
+Reina:"salah satu dari penyihir.. Tapi apakah dia juga ikut dengan para penyihir yang menyerang kerajaan.. "
+
+*Tiba-tiba saja Pintu perpustakaan terbuka dengan kencang*
+
+Kryle:"REINA!! "
+Reina:"WAHH!!ADA APA!?!!"
+Kryle:"... Aku tahu kau menyembunyikan sesuatu dariku"
+Reina:"apa maksud mu?!"
+_(Sial.. Apakah dia tau aku sedang mencari informasi tentang Zavyre?....)_
+
+Kryle:"Salah satu anak buah ku berkata dia mendapatkan sarapan dari mu!!, kenapa kau tidak memberikan ku juga!!"
+Reina:"........._* Plak *_"
+
+Tiba-tiba saja tangan ku bergerak sendiri untuk menampar dirinya, mungkin refleks saat panik. 
+
+Kryle:"AHHH!! UNTUK APA TAMPARAN ITUUU"
+Reina:"tch.. "
+Kryle:"tampara mu itu bisa bertahan sampai 5 jam-.. Shh.. Ahh"
+Reina:"kalo hanya sarapan yang kau inginkan.. Kau bisa mengambil nya sendiri di ruanganku"
+Kryle:"_* Poof *_
+_Dia sudah pergi_
+Reina:"hah... Kenapa dia bisa menjadi pemimpin prajurit disini.. "
+---------------------------------
+*Tiba-tiba ntah bagaimana sebuah buku terjatuh dari salah satu rak buku yang ada disitu.*
+
+Aku mendekati buku yang jatuh itu dan hendak meraihnya untuk di simpan kembali, tetapi mataku tidak sengaja melihat di samping buku itu ada sebuah kertas lusuh yang terlihat sudah lama dibiarkan disitu.
+Aku pun mengambil kertas itu dan ternyata itu adalah sebuah gambar yang terdapat beberapa orang di dalamnya. 
+
+Reina:".. Ini..gambar Ayah dengan teman-teman lama nya... Dan.."
+Reina:"!!!.. Orang ini.. Kenapa sangat mirip dengan.. Zavyre?!? "
+
+Aku kembali melihat buku yang sempat terjatuh tadi dan aku pun tahu bahwa itu adalah buku harian ayahku.
+Akupun memutuskan untuk mencari petunjuk dari buku itu. 
+---------------------------------
+_"Tanggal : 13 April XXXX"_
+-ntah mengapa Zakhir bertingkah aneh akhir-akhir ini, dia terus membicarakan tentang kekuasaan-
+_"Tanggal 20 Desember XXXX"_
+-Aku dan beberapa tempan lain nya  datang ke rumah Zakhir karena dikabarkan dia sudah memiliki seorang putra sekarang.. Tapi Zakhir tidak ada dirumah.. Hanya ada istri dan anaknya yang baru lahir itu, namanya Za... -
+
+Sayangnya tulisan itu tidak bisa ku baca karena sedikit bagian dari kertasnya sobek. 
+---------------------------------
+Reina:"Za... Zavyre?.. Apakah Zakhir itu nama ayahnya?.. Dan jika benar ayahnya adalah salah satu sahabat ayahku.. "
+
+Aku pun membalik beberapa halaman lagi.. Dan hanya ada halaman kosong.. Ayah tidak melanjutkan bukunya?... 
+
+*Setelah beberapa kali membalik halaman akhirnya ada 1 halaman lagi*
+
+_"Tanggal           "_
+-Besok Para penyihir akan datang, Teman-teman ku sudah diculik satu persatu oleh mereka, hanya saja Zakhir.. Aku tidak tahu dimana dia.. -
+---------------------------------
+Reina:"... Tidak ada gambar apapun lagi... Zakhir... Dia sangat mirip dengan Zavyre.. Nama mereka pun memiliki awal yang sama.."
+Reina:"tidak salah lagi Zakhir adalah nama ayah Nya.. Dia pernah bersahabat dengan ayahku.. Tetapi dia menghilang... "
+
+_"Tidak ada petunjuk lain"_
+---------------------------------
+_"18 tahun lalu... Saat itu yang kulihat disana hanyalah jasad orang tua ku... Dan juga kryle yang selalu berdiri di samping ku... "_
+_" Ayo Reina berusahalah untuk mengingat apa lagi yang kau lihat saat itu"_
+_"serangan... Penyihir.. Sinar merah menyala... Dan... Seseorang di ujung sinar merah"_
+_"aku mengingat nya sekarang, seorang pria tinggi dengan rambut putih dan memiliki banyak luka di wajahnya, Zakhir.....dan...Aku telah menghabisinya..."_
 ---------------------------------`,
 };
 
