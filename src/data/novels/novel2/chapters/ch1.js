2986f24388ae1777a0bfab75f7dafd645ddf2038
reinavultador-cloud <reinavultador@gmail.com>
2026-04-14 16:21:45 +0700

    Update ch1.js

--- src/data/novels/novel2/chapters/ch1.js
+++ src/data/novels/novel2/chapters/ch1.js
@@ -1,16 +1,39 @@
 const chapter = {
   id: 1,
-  arc: "Arc 1: Ini placeholder, nanti ganti sendiri",
-  title: "#1 Ini placeholder, nanti ganti sendiri",
+  arc: "Arc 2: Flame Arc",
+  title: "#1 Nama yang Diberkati, Dunia yang Terbakar",
   content: `
 ---------------------------------
-*Ini placeholder, nanti ganti sendiri*
-
-Karakter: "Ini placeholder, nanti ganti sendiri"
-
-> _Ini placeholder, nanti ganti sendiri_
+Rasanya mustahil untuk seorang bayi mengerti apa yang di katakan oleh para orang Dewasa pada mereka saat mereka baru saja terlahir ke dunia, tapi Aku ingat sekali setiap suara lembut, kata-kata yang seakan terasa hangat, dan sentuhan yang begitu membuatku merasa aman pada saat itu. 
 ---------------------------------
-  `,
+???: "Shhh..Lihatlah betapa cantik nya dia saat sedang terlelap"
+???:"heh.. Dia secantik dirimu, aku tidak sabar untuk melihatnya dewasa pasti akan secantik ibunya, mungkin lebih cantik.. "
+???:"Shhh.. Kau ini selalu saja menggodaku.. "
+???:"hahaha.. Kau saja yang sangat mudah untuk digoda"
+
+???:"shh!. Lihat dia mulai membuka matanya"
+???:"Owww... Halo Putriku yang sangat cantik.."
+???:"lihatlah tangan kecilnya yang mencoba untuk meraih mu"
+???:"hehe, halo putri ku.... Kehidupan ini akan sangat sempurna jika dirimu ada disini putriku.. "
+???:"haha kau terlalu berlebihan suamiku"
+???:"aku mengatakan yang sebenarnya istriku haha"
+
+???:"hehe, nama apa yang cocok untuk dia ya.. "
+???:".. Aku sudah memikirkan hal itu.. Putriku _Reina_...... 
+???:"Nama yang cantik... Reina... Seakan Para Bintang pun akan tunduk padanya..... 
+---------------------------------
+Setidaknya itu yang aku ingat... Reina.. Aku yakin orang-orang memanggil ku dengan nama itu.. Reina... Tapi mengapa?
+---------------------------------
+*Suara ricuh mulai terdengar dari segala arah, seakan kebisingan siap untuk melahap semua yang berada dalam jangkauan nya.*
+
+*Semua orang berteriak, menangis, merintih, meratapi nasib mereka masing-masing.*
+
+*Seakan Semua suara yang ada di dunia disatukan dalam satu tempat yang sama, suara ayunan pedang yang saling bertabrakan, suara kuda yang meringik dari segala penjuru, dan suara semua Pria yang berteriak dengan penderitaan.*
+---------------------------------
+Ntah sudah berapa lama aku tidak sadarkan diri, saat aku membuka mataku yang kulihat disekeliling hanyalah tanah yang sudah terbakar seakan dipenuhi oleh bara api yang menyala dengan cahaya yang panas "Dimana aku? ,apa yang terjadi?".. Sebelum aku bisa menjawab pertanyaan yang ada di pikiranku itu...... 
+
+Kryle:"REINA!!BANGUN!!PARA PENYIHIR ITU!!KITA HARUS MELAKUKAN SESUATU!!REINA!!"
+---------------------------------`,
 };
 
 export default chapter;
