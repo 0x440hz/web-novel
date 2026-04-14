0e927b92389d5674c950549c006c5e318f53fb14
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 06:39:29 +0700

    Add chapter 18 to novel2

---
+++ src/data/novels/novel2/chapters/ch18.js
@@ -0,0 +1,11 @@
+const chapter = {
+  id: 18,
+  arc: "Arc 2: Flame Arc",
+  title: "#18 Sumpah Dendam di Balik Luka",
+  content: `
+---------------------------------
+
+---------------------------------`,
+};
+
+export default chapter;
