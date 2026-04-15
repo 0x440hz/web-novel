a9a88c9f022130988e665928d48ccb253f0a4c50
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 11:52:51 +0700

    Update ch27.js

--- src/data/novels/novel2/chapters/ch27.js
+++ src/data/novels/novel2/chapters/ch27.js
@@ -1,10 +1,142 @@
 const chapter = {
-  id: 5,
+  id: 27,
   arc: "Arc 2: Flame Arc",
-  title: "#5 Sumpah Dendam di Balik Luka",
+  title: "#27 Benang Waktu yang Mulai Terungkap",
   content: `
 ---------------------------------
+*Tak terasa mereka sudah berbincang cukup lama di air mancur itu, sampai ada seorang prajurit yang menghampiri mereka berdua*
 
+Kaelous:"aku sudah menyambung setiap benang merah dari perjalanan "keabadian mu".. "
+Reina:"Dan apa yang kau dapatkan?"
+Kaelous:"Kehidupan Reina yang sekarang ini adalah kehidupan ke tiga"
+Reina:"Ke tiga?"
+Kaelous:"benar..dan pertama kali dirimu ada di dunia.. Ada seorang pria yang menemukan mu ditengah hutan.. Tetapi ntah karena alasan apa kalian berdua mengorbankan diri"
+Reina:".... Mm, siapa orang itu"
+Kaelous:"aku tidak bisa mengetahuinya, karna aku hanya bisa memulihkan ingatan mu mulai dari kehidupan ke-2"
+Reina:".. Mungkin harus ada pemantik ingatannya.. Seperti mu.. Kau berasal dari kehidupan ke-2 ku"
+Kaelous:"... Ah!! Aku akan mencatat hal itu..tenang saja Reina aku akan membantu mu mengingat asal usul dirimu"
+---------------------------------
+*Tidak terasa, matahari sudah mulai tenggelam dan cahaya matahari sore itu memantul di air mancur tempat mereka berbincang*
+
+Prajurit 1:"Hey..menurut mu siapa yang berbicara dengan putri sejak pagi? "
+Prajurit 2:"ntahlah, mereka sudah disana membicarakan banyak hal sejak pagi.. Mungkin itu seseorang yang sudah lama tidak bertemu dengan putri"
+Prajurit 1:"Hah..syukurlah Putri sudah mau keluar dan berbicara dengan orang lain.."
+Prajurit 2:"kau benar tapi sudah saatnya mengantarkan Putri kembali"
+
+*Kedua Prajurit itu menghampiri Reina*
+
+Prajurit:"Per Fidem et Gloriam, putri Reina, maaf mengganggu waktu anda, tetapi hari sudah mulai senja, sudah menjadi tugas kami untuk mengantarkan mu kembali dengan aman"
+Reina:"oh... Terimakasih, aku terlalu terbawa suasana hingga tidak ingat waktu"
+Kaelous:"kau punya orang-orang yang peduli padamu disini.. "
+
+*kedua Prajurit itu berdiri menunggu respon Reina untuk kembali ke istana*
+
+Reina:"baiklah.. Seperti nya memang sudah waktunya untuk kembali"
+
+*Reina dan Kaelous langsung berdiri dan hendak berpisah*
+
+Kaelous:"waktu nya kembali Reina"
+Reina:"apa kau akan pulang?"
+Kaelous:"ya.. Tidak ada pilihan lain kan? "
+Reina:"tentu saja ada.. "
+Kaelous:"Eh?"
+Prajurit:"He?"
+
+*Reina menengok ke arah kedua prajurit itu*
+Reina:"Dia boleh tinggal di istana kan?"
+Prajurit 1:"T-tapi..putri.."
+Prajurit 2:"Um.. Dia butuh izin dari kapten Lioren-"
+Kaelous:"t-tidak perlu sampai seperti itu Reina.. 
+Reina:".. Ayolah aku mohon"
+Prajurit 1:"T-tapi..."
+
+*Reina langsung menggenggam tangan Prajurit itu dan mengangkatnya hingga setara dengan bibirnya lalu memohon*
+
+Reina:"Aku mohon.."
+*Prajurit itu tidak bisa menolak tindakan Reina yang sangat..*
+
+Prajurit 1:("l-lucu sekali") 
+Prajurit 2:"Hey...kendalikan dirimu"
+Kaelous:"Itu dia Reina yang aku kenal"
+
+Prajurit 2:"Baiklah putri Reina.. Keinginan mu adalah perintah untuk kami"
+Prajurit 1:"kami- akan mengantar kalian berdua"
+Kaelous:"Wah terima kasih banyak"
+Reina:"hehehe"
+---------------------------------
+*setibanya di gerbang istana*
+Lioren:"SIAPA DIA!!!"
+Vyerna:"psst.. Hai tampan~"
+*Vyerna langsung menghampiri Kaelous dengan tingkah ularnya yang terlihat licik seperti biasanya*
+Lioren:"V-vyerna kau bilang aku satu-satunya pria yang kau sebut tampan"
+Kaelous:"a.. Ahaha Terimakasih atas pujian mu"
+--------------------------
+#2*Tak terasa mereka sudah berbincang cukup lama di air mancur itu, sampai ada seorang prajurit yang menghampiri mereka berdua*
+
+Kaelous:"aku sudah menyambung setiap benang merah dari perjalanan "keabadian mu".. "
+Reina:"Dan apa yang kau dapatkan?"
+Kaelous:"Kehidupan Reina yang sekarang ini adalah kehidupan ke tiga"
+Reina:"Ke tiga?"
+Kaelous:"benar..dan pertama kali dirimu ada di dunia.. Ada seorang pria yang menemukan mu ditengah hutan.. Tetapi ntah karena alasan apa kalian berdua mengorbankan diri"
+Reina:".... Mm, siapa orang itu"
+Kaelous:"aku tidak bisa mengetahuinya, karna aku hanya bisa memulihkan ingatan mu mulai dari kehidupan ke-2"
+Reina:".. Mungkin harus ada pemantik ingatannya.. Seperti mu.. Kau berasal dari kehidupan ke-2 ku"
+Kaelous:"... Ah!! Aku akan mencatat hal itu..tenang saja Reina aku akan membantu mu mengingat asal usul dirimu"
+---------------------------------
+*Tidak terasa, matahari sudah mulai tenggelam dan cahaya matahari sore itu memantul di air mancur tempat mereka berbincang*
+
+Prajurit 1:"Hey..menurut mu siapa yang berbicara dengan putri sejak pagi? "
+Prajurit 2:"ntahlah, mereka sudah disana membicarakan banyak hal sejak pagi.. Mungkin itu seseorang yang sudah lama tidak bertemu dengan putri"
+Prajurit 1:"Hah..syukurlah Putri sudah mau keluar dan berbicara dengan orang lain.."
+Prajurit 2:"kau benar tapi sudah saatnya mengantarkan Putri kembali"
+
+*Kedua Prajurit itu menghampiri Reina*
+
+Prajurit:"Per Fidem et Gloriam, putri Reina, maaf mengganggu waktu anda, tetapi hari sudah mulai senja, sudah menjadi tugas kami untuk mengantarkan mu kembali dengan aman"
+Reina:"oh... Terimakasih, aku terlalu terbawa suasana hingga tidak ingat waktu"
+Kaelous:"kau punya orang-orang yang peduli padamu disini.. "
+
+*kedua Prajurit itu berdiri menunggu respon Reina untuk kembali ke istana*
+
+Reina:"baiklah.. Seperti nya memang sudah waktunya untuk kembali"
+
+*Reina dan Kaelous langsung berdiri dan hendak berpisah*
+
+Kaelous:"waktu nya kembali Reina"
+Reina:"apa kau akan pulang?"
+Kaelous:"ya.. Tidak ada pilihan lain kan? "
+Reina:"tentu saja ada.. "
+Kaelous:"Eh?"
+Prajurit:"He?"
+
+*Reina menengok ke arah kedua prajurit itu*
+Reina:"Dia boleh tinggal di istana kan?"
+Prajurit 1:"T-tapi..putri.."
+Prajurit 2:"Um.. Dia butuh izin dari kapten Lioren-"
+Kaelous:"t-tidak perlu sampai seperti itu Reina.. 
+Reina:".. Ayolah aku mohon"
+Prajurit 1:"T-tapi..."
+
+*Reina langsung menggenggam tangan Prajurit itu dan mengangkatnya hingga setara dengan bibirnya lalu memohon*
+
+Reina:"Aku mohon.."
+*Prajurit itu tidak bisa menolak tindakan Reina yang sangat..*
+
+Prajurit 1:("l-lucu sekali") 
+Prajurit 2:"Hey...kendalikan dirimu"
+Kaelous:"Itu dia Reina yang aku kenal"
+
+Prajurit 2:"Baiklah putri Reina.. Keinginan mu adalah perintah untuk kami"
+Prajurit 1:"kami- akan mengantar kalian berdua"
+Kaelous:"Wah terima kasih banyak"
+Reina:"hehehe"
+---------------------------------
+*setibanya di gerbang istana*
+Lioren:"SIAPA DIA!!!"
+Vyerna:"psst.. Hai tampan~"
+*Vyerna langsung menghampiri Kaelous dengan tingkah ularnya yang terlihat licik seperti biasanya*
+Lioren:"V-vyerna kau bilang aku satu-satunya pria yang kau sebut tampan"
+Kaelous:"a.. Ahaha Terimakasih atas pujian mu"
 ---------------------------------`,
 };
 
