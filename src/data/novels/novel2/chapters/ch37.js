9e3bd94462077b711fda4bd75ae40cef694b5b42
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 18:53:14 +0700

    Create ch37.js

---
+++ src/data/novels/novel2/chapters/ch37.js
@@ -0,0 +1,11 @@
+const chapter = {
+  id: 31,
+  arc: "Arc 2: Flame Arc",
+  title: "#31 Kenangan yang Tak Siap Dilepaskan",
+  content: `
+---------------------------------
+
+---------------------------------`,
+};
+
+export default chapter;
