0824230e89bf3d8cc6d2ce6263ec5f89792650cf
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 18:50:16 +0700

    Create ch31.js

---
+++ src/data/novels/novel2/chapters/ch31.js
@@ -0,0 +1,82 @@
+const chapter = {
+  id: 31,
+  arc: "Arc 2: Flame Arc",
+  title: "#31 Kenangan yang Tak Siap Dilepaskan",
+  content: `
+---------------------------------
+*Jauh Dari tanah kerajaan Vulkrath, ada sebuah reruntuhan tak bernama yang telah lama di tinggalkan, namun menjadi saksi bisu atas aktivitas misterius yang memiliki hubungan dengan hilangnya Botol dari Gua Zavyre*
+
+*Didalam reruntuhan, terbaring tubuh yang memiliki rambut putih lusuh, dengan sehelai rambut merah, dan mata hitam yang kehilangan cahayanya*
+
+???:"Akhirnya, bahan yang paling penting dari semuanya... Bahan terakhir agar proses pembangkitan kembali ini berhasil"
+
+*Angin dingin berputar kencang. Rambut putih lusuh di tubuh itu terangkat seolah tertiup napas tak kasat mata. Retakan cahaya hitam mulai merayap di kulitnya.*
+--------------------------------------
+*Sementara di Istana*
+
+*Reina tersentak dari duduknya, dadanya terasa sesak. Udara di sekitarnya bergetar aneh.*
+
+Reina:"…Tidak mungkin..... Zavyre?"
+Zavyre:"... Reina..."
+
+*Suaranya terdengar dari belakang Reina, dan membuat Reina merinding*
+
+Reina:"Zavyre?!.. ZAVYRE?!"
+
+*tiba-tiba ada suara ketukan dari pintu kamarnya*
+Vyerna:"Reina... Ini aku"
+Reina:"..... *
+
+*Reina membukakan Pintu dan melihat wajah Vyerna yang agak ketakutan, dan khawatir*
+
+Reina:"Vyerna ada apa?.."
+Vyerna:"Reina.. Sebenarnya...-"
+
+*Dia menceritakan apa yang sudah terjadi di malam itu*
+
+Vyerna:"Aku khawatir... Karna Aku sempat merasakan sihir Zavyre... Maka dari itu aku memutuskan untuk memberitahukan ini padamu
+Reina:"Terimakasih sudah memberitahuku Vyerna"
+Vyerna:"Maaf Aku sudah berniat untuk tidak memberitahukan dirimu tentang ini, Aku hanya ingin memastikan kau tidak... "
+Reina:"Sudah... Tidak apa-apa... Pada akhirnya semua akan kembali seperti dulu ya... Hehe.. Aku akan sangat merindukan nya"
+---------------------------------
+*Kaelous tiba-tiba datang saat mereka berdua sedang berbicara, dan mereka langsung terdiam*
+
+Kaelous:"Rei.. Na-, oh ternyata Kau sedang ada disini Vyerna"
+Vyerna:"ah Aku akan segera pergi tenang saja.. Aku hanya menyampaikan sesuatu pada Reina"
+Reina:"hehe.. Benar, dia hanya bertanya berapa banyak kamar yang harus dia buat untuk anak-anak nya dengan Lioren"
+Vyerna:"APA-!?! REINA?!!?!"
+Kaelous:"wah.. Wah... "
+Reina:"Hehe.. Aku hanya bercanda"
+
+*Secara sangat kebetulan Lioren dan dua prajurit lewat di depan kamar Reina dan langsung berteriak dari luar kamar sambil berjalan melewatinya*
+
+Lioren:"TIGA!"
+Prajurit:"TIGA! TIGA! TIGA!"
+
+*Reina dan Kaelous langsung tertawa saat mendengar teriakan mereka di sepanjang lorong*
+
+Vyerna:"Apa-apaan mereka itu.. Huff"
+Kaelous:"haha.. Tidak apa-apa lagi pula Aku dengar dari Lioren pernikahan kalian sudah dekat bukan?"
+Reina:".. Benar begitu?.. Dasar, Lioren tidak memberitahukan ku apapun"
+
+Vyerna:"A-Aku akan keluar sekarang.. Kalian berdua habiskan waktu kalian-"
+*Vyerna hendak pergi keluar dan diam di depan pintu sebentar untuk melihat ke Reina*
+
+Vyerna:"Terimakasih Reina"
+Reina:"Aku akan menantikan Pertemuan itu"
+
+*Pintu kamar tertutup perlahan setelah Vyerna pergi. Suasana yang tadinya penuh tawa kembali hening.*
+
+Kaelous:"Nah Reina.. Aku kemari untuk janjiku"
+Reina:"hah?.. Janji apa!?"
+Kaelous:"Jangan pura-pura polos putri"
+
+*Kaelous berjalan mendekat ke arah Reina*
+
+Reina:"E-EHHHHHH, K-KAEL.."
+Kaelous:"Ssshh~.. Jangan sampai ada yang mendengar suaramu seperti itu
+Reina:"HIH-!"
+---------------------------------`,
+};
+
+export default chapter;
