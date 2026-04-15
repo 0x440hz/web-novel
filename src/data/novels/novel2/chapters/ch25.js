1b6c72e5109d22fef3d2424e84a0147e7739b158
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 11:37:00 +0700

    Update ch25.js

--- src/data/novels/novel2/chapters/ch25.js
+++ src/data/novels/novel2/chapters/ch25.js
@@ -1,10 +1,202 @@
 const chapter = {
-  id: 5,
+  id: 25,
   arc: "Arc 2: Flame Arc",
-  title: "#5 Sumpah Dendam di Balik Luka",
+  title: "#25 Cahaya Terakhir dari Segala Pengorbanan",
   content: `
 ---------------------------------
+*Tiba-tiba anak panah muncul dan menghancurkan Cermin yang mengelilingi mereka berdua*
 
+Lioren:"PUTRI REINA! SERANG MAYAT-MAYAT ITU TERLEBIH DAHULU!"
+
+*Reina menoleh ke arah Lioren dan mengangguk*
+
+Zavyre:"Aku akan melakukannya untuk mu Reina"
+
+*Zavyre mulai menyerang mayat-mayat yang melindungi Altheas dengan serangan yang sangat besar*
+
+Reina:"Aku mengandalkan mu Zavyre"
+
+*Zavyre menghantam mayat-mayat penyihir dengan serangan bertubi-tubi. Setiap tebasannya memutus rantai darah yang mengikat mereka pada Altheas.*
+
+Zavyre:"KAU TIDAK PANTAS JADI BAGIAN DARI KELUARGAKU LAGI ALTHEAS!!"
+
+*Zavyre menghantam mayat-mayat penyihir dengan serangan bertubi-tubi. Setiap tebasannya memutus rantai darah yang mengikat mereka pada Altheas.*
+
+*Langit bergemuruh.Tanah bergetar. Dunia terasa seperti menahan napas,Altheas mulakehilangan kestabilannya.*
+
+Altheas:"TCH!!LIHAT SIAPA YANG BERBICARA!!"
+
+Altheas:"_SPECULUM LANCEA-IMPALIS RUPTURE"_
+*Banyak tombak yang terbuat dari cermin tajam muncul di udara.Semua mengarah ke Zavyre lalu menusuk serempak.*
+
+Zavyre:"!!!!.._"SCUTUM DRACOLIS"_
+Zavyre sempat membuat pertahanan untuk tubuhnya. 
+
+*Setelah menembus tubuh Zavyrre, pecahan cermin meledak dari dalam tubuhnya*
+
+*Pertahanan itu memang tidak terlalu kuat tapi cukup untuk melindungi Zavyre dari serangan yang bisa saja membunuhnya*
+
+Reina:"ZAVYRE!!!!"
+Zavyre kehilangan keseimbangannya dan hampir terjatuh tapi Reina langsung menangkap Tubuhnya. 
+
+Reina:"Zavyre!"
+Zavyre:"Aku tidak apa-apa.. Lihat mayat-mayat itu sudah hilang semua, Kekuatan Altheas sudah berkurang dengan pesat"
+---------------------------------
+*Kryle melihat kearah Altheas yang mulai kehilangan sumber kekuatannya*
+
+Kryle:"Dia.... Boleh juga"
+
+*Tiba-tiba Tanah yang di pijak oleh Kryle dan prajurit lain bergetar seakan ada sesuatu yang hendak muncul dari bawah tanah*
+
+Kryle:"TCH.. APA INI-"
+Lioren:"KAPTEN! HATI-HATI!!"
+
+*Dan benar saja, sebuah mahluk besar ciptaan Altheas muncul dari bawah tanah, makhluk itu berbentuk Seperti naga berwarna gelap dan memiliki mata merah terang yang terlihat seperti api*
+
+*Lioren yang sadar akan kemunculan itu langsung melompat dan mendorong Kryle dari tempat nya berdiri sehingga mereka bisa menghindari makhluk itu*
+
+Altheas:"LIHATLAH KE BAWAH SANA REINA!!! AKU TAHU DIMANA KELEMAHAN MU"
+
+Reina:_"PLUVIA LUMEN"_
+*Hujan Bilah Cahaya Turun di atas Altheas, beberapa bilah cahaya itu mengenai Altheas dan juga Naga kegelapan di bawahnya*
+
+*Zavyre melihat peluang untuk mengakhiri Altheas langsung maju kearahnya*
+
+Zavyre:"RASAKAN INI WANITA KEPARAT!!!"
+Reina:"ZAVYRE JANGAN!!!!"
+Zavyre:_"SANGUIS NOCTIS"_
+Ditangannya Muncul sebuah Tombak tajam yang terbuat dari Darahnya sendiri, dan langsung mengayunkan nya ke arah Leher Altheas
+
+Altheas:"Dasar Payah"
+*Begitu Zavyre berjarak cukup dekat dengan Altheas dia langsung menusuk tepat di jantung Zavyre dengan tangan yang terbalut Kaca panas dan tajam*
+
+Reina:"TIDAK!!!"
+
+Altheas:_"VITRUM IGNIS"_
+Zavyre:"......-"
+Altheas tersenyum ke arah Zavyre yang masih menancap di Tangannya. 
+Altheas:"kau memang bodoh.. Keponakan ku"
+Lalu Altheas menjatuhkan Zavyre dari ketinggian. 
+
+Reina:".... Tidak! Zavyre!!"
+Kryle:"AKU AKAN MENANGKAPNYA REINA!"
+
+*Tiba-tiba Naga kegelapan muncul dibelakang Kryle dan langsung mencabik-cabik Tubuh kryle dengan brutal*
+
+Kryle:"!!!!!"
+Altheas:"heh... "
+
+*Tubuh Zavyre dan Kryle akhirnya jatuh ke tanah*
+*naga yang mencabik-cabik Kryle menghampiri Altheas dan langsung menyatu dengan dirinya*
+
+Reina:"tidak.... tidak... TIDAK!!! _Celeris!!_"
+*Reina langsung menghampiri Kryle yang sudah terluka hebat, darah mengalir dari seluruh tubuhnya.*
+
+*Sementara Zavyre yang juga Terjatuh di dekat mereka perlahan-lahan memudar menjadi abu yang berterbangan di udara*
+---------------------------------
+Reina:"Tidak tidak, kryle!!"
+*Reina Mengguncang tubuh Kryle yang terlihat sudah lemah tidak bergerak sedikit pun. *
+
+*Dengan kekuatan yang tersisa Zavyre berdiri dan menghampiri Reina*
+
+Zavyre:"R.. Rei..."
+Setelah mendekati Reina Zavyre terjatuh di pangkuannya. 
+
+Reina:"Zavyre... Apa yang!?!.. Mengapa kau seperti ini Zavyre"
+
+Kryle:"Reina....."
+Dengan suara pelan yang lemah Kryle memanggil namanya
+
+Reina:"K-Kryle!??, tenanglah kau dan Zavyre akan baik-baik saja!"
+
+Kryle:"tidak Reina.. Kau bisa mengalahkannya"
+Reina:"tidak tanpa kalian berdua"
+Zavyre:"Kau bisa melakukannya...Reina"
+
+Kryle:"Buktikan jika keabadian mu itu bukanlah Kutukan"
+Zavyre:"Keabadian mu itu adalah Anugerah.."
+
+*Zavyre semakin memudar, dan napas kryle mulai melemah*
+
+Kryle:"Keabadian mu bukan karena Dosa"
+Zavyre:"Melainkan Dari harapan semua makhluk yang bernyawa"
+
+_"Kau lahir dari Harapan semua mahluk"_
+
+_"Kau adalah Harapan Ku.... Reina"_
+---------------------------------
+*Semua suara tiba-tiba menghilang, hanya ada keheningan yang berisik untuk sesaat*
+
+*Tubuh Zavyre menghilang diudara*
+
+*Tubuh Kryle kehilangan Gerakannya*
+
+*Prajurit Vulkrath berkurang dengan pesat*
+
+*Lioren dan Vyerna sudah terbujur Kaku ditanah*
+
+_Sisa Diriku sendiri, dan Altheas_
+---------------------------------
+Altheas:"REINA!!!! INI SEMUA SALAHMU!!KEHANCURAN VULKRATH!!!, KEMATIAN SEMUA ORANG, ITU SEMUA SALAH MU!!!!"
+
+*Altheas meneriaki Reina yang sedang memegangi tangan Kryle di tanah*
+
+_"Ah... Suara wanita itu berisik sekali"_
+
+*Muncul bola Cahaya di sekelilingnya Reina yang hendak berdiri,dia memiliki sinar sangat terang.*
+
+*Bukan lagi sekadar emas.Melainkan putih menyilaukan bercampur emas murni,berdenyut seperti matahari yang turun ke bumi.*
+
+_"Celeris"_
+*Reina berhadapan langsung dengan Altheas*
+
+Altheas:"Akhirnya kau berani untuk muncul dihadapanku.. Reina!"
+
+*Reina kemudian berbicara  bukan dengan teriakan, tetapi dengan suara yang menggema di seluruh langit*
+
+_"Cahaya yang lahir dari pengorbanan…"_
+_"Cahaya yang menanggung kesedihan…"_
+_"Cahaya yang membakar dosa dunia…"_
+
+*Langit terbuka.*
+
+*Lingkaran sihir raksasa muncul di atas Reina lebih besar dari milik Altheas.*
+
+*Berwarna putih keemasan, berputar seperti roda takdir yang tak bisa dihentikan.*
+
+Altheas:("kekuatan besar apa ini-") 
+
+*Reina menatap lurus ke Altheas.*
+
+_“Dengan nama mereka yang gugur…Dengan nama Vulkrath…Dengan nama takdirku sendiri…"_
+
+*Cahaya membanjiri tubuh Reina.*
+
+*Tiba-tiba tubuh Altheas terasa berat seakan di tekan oleh kekuatan yang sangat besar*
+*Muncul kegelisahan di wajah Altheas*
+
+Altheas:"AKU TIDAK AKAN KALAH REINA!!!!"
+Altheas:_"SPECULUM ABSOLUTUS - AEGIS REFLECTA"_
+
+*Altheas mengangkat kedua tangannya ke langit.*
+
+*Di sekelilingnya muncul tiga lapis lingkaran sihir cermin raksasa berwarna hitam-keperakan, berputar berlawanan arah satu sama lain*
+
+Reina:_"LUMEN APOCALYPSIS -AURORA TERMINUS!!!"_
+*Cahaya raksasa turun dari langit seperti pilar matahari yang menghancurkan segalanya.*
+
+*Altheas menjerit Kesakitan*
+
+*Tubuhnya terbakar oleh cahaya yang tidak bisa ia lawan.*
+
+_Tidak ada bayangan._
+_Tidak ada kegelapan._
+_Tidak ada rantai darah._
+_Hanya cahaya mutlak._
+
+Altheas:"TIDAK AKU TIDAK AKAN KALAAAAHHHHH!!!!!!"
+
+*Namun terlambat tubuh Altheas hancur menjadi debu cahaya hitam yang lenyap ditelan langit.*
 ---------------------------------`,
 };
 
