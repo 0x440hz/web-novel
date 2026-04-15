17965398c82d0ce83a1c732bd8972359b27679ee
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 07:07:53 +0700

    Update ch16.js

--- src/data/novels/novel2/chapters/ch16.js
+++ src/data/novels/novel2/chapters/ch16.js
@@ -1,10 +1,66 @@
 const chapter = {
   id: 16,
   arc: "Arc 2: Flame Arc",
-  title: "#16 Sumpah Dendam di Balik Luka",
+  title: "#16 Detak Jantung yang Lebih Keras dari Pedang",
   content: `
 ---------------------------------
+_"Apa yang sebenarnya terjadi padaku?"_
+---------------------------------
+Setelah Pertarungan malam itu, aku selalu merasa gelisah..ntah apa yang akan terjadi di kemudian hari, aku tidak tahu bagaimana caraku selanjutnya untuk mempertahankan kerajaan ini. 
+---------------------------------
+Terdengar suara ketukan pintu dari luar kamar Reina diikutin oleh suara Kryle yang lembut. 
 
+Kryle:"Reina.. Kau didalam?"
+Reina:"Masulah kryle, aku tidak mengunci pintunya "
+
+Kryle membuka pintu dan masuk dan menemui Reina yang sedang terduduk di kasurnya. 
+
+Reina:"ada apa kryle?..tidak seperti biasanya kau datang di pagi hari seperti ini.."
+Kryle:"aku hanya ingin memastikan jika kau baik-baik saja Reina.. "
+Reina:"Kemari dan duduklah"
+
+Kryle melangkah maju kearah Reina dan hendak meraih kursi dari meja Reina.
+
+Reina:"tidak perlu kursi.. Duduklah di sini"
+Kryle:"t-tapi itu kasur mu.. Bagaimana jika terkena noda-"
+Reina:"tidak apa-apa.. Kemari"
+
+Reina tersenyum sambil menepuk sisi kasurnya mengisyaratkan kryle untuk duduk di dekatnya. 
+
+Kryle:"mm... Bagaimana luka-luka mu Reina?"
+Reina:"sudah membaik, sihir penyembuhan dari para pelayan istana membuatnya lebih cepat sembuh"
+Kryle:"syukurlah.. "
+
+*mereka saling bertukar senyuman untuk itu*
+*namun untuk Sesaat setelah perbincangan singkat itu semuanya hening untuk beberapa seakan masing-masing dari mereka menunggu untuk memecah keheningan canggung itu*
+
+Dan akhirnya kryle memutuskan untuk memecah keheningan itu. 
+----------------------------------
+Kryle:"REINA-"
+Reina:"Ah-... Um?.. Iya kryle?"
+
+Kryle:"ah... Begini- (sial,kenapa aku tiba-tiba berteriak.. Baiklah, tenang saja cukup katakan apa yang ingin kau katakan kryle)"
+Reina:"kryle?"
+Kryle:"Reina.. Begini... Bersedia kah dirimu untuk men-.. Men.. 
+Reina:"Men?... "
+Kryle:"MENEMANIKU UNTUK LATIHAN BERPEDANG!!!"
+
+Untuk sementara Reina terdiam dan masih menatap ke arah kryle yang telinganya terlihat sedikit memerah. 
+
+Reina:"hahah.. Aku kira kau ingin mengatakan sesuatu yang penting.. Boleh saja, sudah lama juga bukan?"
+
+Kryle hanya membalas dengan anggukan kepala. 
+
+Kryle:"k-kalau begitu aku akan menunggu mu di tempat latihan-"
+Reina:"Baiklah"
+
+Kryle berdiri dan terburu-buru meninggalkan kamar Reina, dengan wajah yang sudah sangat merah. 
+
+Kryle:"Waaaahh payah sekaliiii!!!"
+
+Reina terdiam didalam kamarnya melihat kepergian kryle yang tergesa-gesa. 
+
+Reina:"Apa yang salah dengannya... -"
 ---------------------------------`,
 };
 
