ece08eba8b74b3fecb9b6d4686f787f83ffb4789
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 11:58:24 +0700

    Update chapter details for chapter 30

--- src/data/novels/novel2/chapters/ch30.js
+++ src/data/novels/novel2/chapters/ch30.js
@@ -1,10 +1,92 @@
 const chapter = {
-  id: 5,
+  id: 30,
   arc: "Arc 2: Flame Arc",
-  title: "#5 Sumpah Dendam di Balik Luka",
+  title: "#30 Dia yang Menunggumu Sepanjang Waktu",
   content: `
 ---------------------------------
+*Keesokan paginya, Vyerna masih belum menceritakan yang terjadi di malam sebelumnya pada siapapun termasuk Lioren*
 
+Vyerna:"... Cepat atau lambat Reina akan merasakan energi sihir asing mendekat"
+Vyerna:"bagaimana aku harus menjelaskannya.. "
+---------------------------------
+*Sementara itu Reina sedang duduk sendirian di taman desa melihat aktivitas para penduduk yang bisa di bilang, aman*
+
+*Beberapa prajurit berjaga sempat melihat Reina dan menyapa seperti biasanya, begitu juga para penduduk Vulkrath*
+
+_??:"Tidak baik seorang putri kerajaan duduk sendirian di tengah keramaian seperti ini bukan..Reina?"_
+
+Reina:"!!?!"
+
+*Reina tersentak saat mendengar suara yang pelan nyaris seperti berbisik Tiba-tiba hadir di tengah suara keramaian orang-orang*
+
+Reina:"..... Zavy-.."
+Kaelous:"Reina!"
+*Reina menoleh kearah kaelous yang memanggilnya dari kejauhan*
+
+Reina:"Kaelous?..bagaimana kau tahu aku ada disini?"
+Kaelous:"Aku mengikuti Benang merah tak kasat mata yang terhubung antara kita berdua~"
+Reina:".....hm?"
+Kaelous:"hehe aku hanya bercanda.. Aku menanyakan keberadaan mu pada beberapa prajurit yang sedang berjaga disekitar sini"
+Kaelous:"jadi.. Apa yang kau lakukan disini?"
+Reina:"tidak ada.. Aku hanya berpikir aku sudah lama tidak dekat dengan para penduduk desa"
+Kaelous:"Begitu..baguslah kau ingin pergi keluar untuk beberapa saat"
+Reina:"Apa maksudnya itu... -"
+Kaelous:"hahaha.. Aku hanya bercanda Reina"
+
+*Kaelous duduk di sampingnya,dan menaruh tangannya di belakang Reina*
+
+Kaelous:"Reina.. Ada satu hal yang belum sempat aku beritahu padamu"
+Reina:"Apa itu?..."
+Kaelous:"Kau tau.. Garis waktu di sini dan di kehidupan ke-2 jelas berbeda benar?..."
+Reina:"... Ya?"
+Kaelous:"Sebenarnya Reina.. Saat itu.. Terakhir aku melihat dirimu kau tiba-tiba menghilang di depan mataku"
+Reina:"... Maksudmu.. Saat ledakan sihir terjadi?"
+Kaelous:"ya... Ntah mengapa disaat sihir yang kuat mengalir dimana-mana kau tiba-tiba menghilang tepat di depan mataku"
+Kaelous:"Aku mulai mencari keberadaan mu sejak saat itu.. Dari mulai mencari di kehidupan itu, sampai akhirnya aku menemukan petunjuk tentang kehidupan mu yang ke-3 ini, di Vulkrath"
+Reina:"sudah berapa lama kau mencari?"
+Kaelous:"68 tahun..."
+Reina:".... 68 tahun!?!?! Ituu.. Sangat lama.."
+Kaelous:"ya.. Sejak itu aku hanya terus mencari dan mencari... Hari demi hari... Hingga tahun ke tahun.. Aku menua... Dan hasil dari pencarian 68 tahun disitulah aku sudah bisa menarik benang merah untuk kesimpulan.."
+Kaelous:"selama kurang lebih 2 tahun aku menarik kesimpulan semuanya, hingga akhirnya... Aku menemukan kehidupan mu itu di Vulkrath dan juga beberapa bulan setelahnya.... Aku"
+
+*Kaelous terdiam sesaat mencari cara yang tepat untuk menyampaikan hal terakhir pada reina*
+
+*Kaelous menarik napas, dan mengatakan apa yang ingin dia katakan*
+
+Kaelous:"... Aku Tiada, Karna sakit dan juga faktor usia"
+
+*Reina Terdiam dengan apa yang baru saja dia dengar dari Kaelous*
+
+Reina:"Kau mencari ku selama itu....... Hanya untuk... Aku?!"
+Kaelous:"Benar Reina... Karna saat itu aku berpikir.. Bahwa hanya dirimu lah yang bisa membuatku merasa lengkap"
+
+*Reina tidak mengatakan apapun lagi,hanya saja dia memberanikan dirinya untuk memeluk Kaelous atas dasar keinginannya sendiri*
+
+Kaelous:"R-.. Reina?"
+Reina:"Terima kasih, Kaelous"
+
+*Kaelous membalas pelukan itu dengan pelukan yang lebih erat dari sebelumnya, dan dia meletakan kepalanya di leher Reina.. Dengan itu dia bisa mencium Aroma tubuh Reina yang sangat khas seperti bunga emas yang selalu ia cium di masa lalu*
+---------------------------------
+*Dari kejauhan, taman desa tetap terlihat damai. Tawa anak-anak, pedagang yang memanggil pembeli, dan burung-burung yang beterbangan di udara pagi.*
+
+Prajurit 1:"Hey,Itu… Putri Reina, kan?"
+Prajurit 2:"Iya. Dan pria di sampingnya… Kaelous."
+Prajurit 1:"... Mereka berpelukan."
+Prajurit 2:"huff..Kapten Lioren bakal dapat serangan jantung kalau lihat ini."
+Prajurit 1:"Pfftt..ada benarnya juga"
+Prajurit 2: Kapten pasti akan berkata.. "Oh tidakkk~ putriku Reina~Seharusnya aku yang ada disana~awwwwwhhhh~"
+*mereka berdua tertawa bersama namun Tiba-tiba di belakang mereka berdua ada tangan yang memegang pundak mereka masing-masing diikuti dengan suara yang tidak asing di telinga mereka*
+Lioren:"Hey~Kapten Mana yang kalian maksud itu Hah..?"
+*kedua prajurit itu langsung diam dan menoleh sedikit kebelakang untuk melihat wajah yang ada dibelakang mereka*
+
+*Dia tersenyum, tapi urat di pelipisnya sedikit menegang. Sudut bibirnya terangkat manis, namun matanya memancarkan kejengkelan yang jelas,senyum yang terlalu rapi untuk disebut tulus.*
+
+Prajurit 1:".....habis sudah"
+Prajurit 2:"Senang mengenalmu Kawan "
+
+*Sementara itu di kejauhan Maeryn yang sejak tadi mendengarkan mereka hanya bisa menghela napas saat Lioren menghampiri mereka berdua*
+
+Maeryn:"Hah.. Dasar Pria"
 ---------------------------------`,
 };
 
