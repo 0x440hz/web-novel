7e6369b37c20172b27b8c6d303fb8cad9cae2b2f
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 11:44:46 +0700

    Create ch29.js

---
+++ src/data/novels/novel2/chapters/ch29.js
@@ -0,0 +1,11 @@
+const chapter = {
+  id: 5,
+  arc: "Arc 2: Flame Arc",
+  title: "#5 Sumpah Dendam di Balik Luka",
+  content: `
+---------------------------------
+
+---------------------------------`,
+};
+
+export default chapter;
