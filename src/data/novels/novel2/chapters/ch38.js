05ba94d78f7bcfbe2937a6c0e89c9d6982dc9208
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 19:12:51 +0700

    Update ch38.js

--- src/data/novels/novel2/chapters/ch38.js
+++ src/data/novels/novel2/chapters/ch38.js
@@ -1,10 +1,125 @@
 const chapter = {
-  id: 31,
+  id: 38,
   arc: "Arc 2: Flame Arc",
-  title: "#31 Kenangan yang Tak Siap Dilepaskan",
+  title: "#38 Malam di Mana Vulkrath Kembali Terbakar",
   content: `
 ---------------------------------
+*Lioren dan para prajurit yang berada di hutan mencari asal dari sihir yang tiba-tiba muncul, namun setelah beberapa saat pencarian mereka tidak bisa menemukan dimana sumber sihir tersebut*
 
+Prajurit:"Kapten, kamu sudah menyusuri hutan dengan radius 10 meter dan tidak ada tanda sesuatu atau seseorang yang bisa mengakibatkan ledakan sihir"
+Lioren:"... Hm, baiklah aku rasa itu ledakan sihir yang cukup besar namun tipis sehingga bisa langsung menghilang"
+Prajurit:"Kami menunggu petunjuk mu berikutnya kapten"
+Lioren:"Kita harus segera kembali ke kerajaan"
+
+*semua prajurit setuju dan mereka semua hendak kembali ke Vulkrath, namun sesaat setelah mereka berbalik badan, tiba-tiba ada ledakan sihir gelap yang sangat besar dari arah Vulkrath*
+
+Prajurit:"KAPTEN!!!"
+Lioren:"KITA HARUS CEPAT!"
+
+(Lioren:"Sial bagaimana bisa keamanan di tembus dengan mudah")
+
+*beberapa menit kemudian mereka akhirnya sudah kembali ke kerajaan disana banyak mahluk kegelapan yang mulai mendatangi mereka*
+
+Lioren:"KITA BAGI KELOMPOK, BANTU WARGA DESA, DAN SISANYA KITA SERANG MAHKLUK INI!"
+Prajurit:"SOLAN!"
+
+*Terjadi kekacauan yang besar di kerajaan pada malam itu*
+---------------------------------
+*Di dalam istana, Reina menghampiri Vyerna dan Kaelous ke kamar mereka masing-masing dan mengajak mereka untuk diam di tempat yang lebih aman*
+
+Reina:".. Vyerna, Kaelous.. Kalian baik-baik saja bukan?"
+Vyerna:"A-aku baik-baik saja"
+Kaelous:"apa yang terjadi Reina? .."
+Reina:"Beberapa menit yang lalu aku merasakan sihir yang datang dengan cepat, untungnya aku berhasil membuat penghalang agar tidak terjadi kerusakan yang terlalu banyak"
+Vyerna:"... Sihir ini.. Zavyre!"
+Kaelous:"Zavyre!?.. Bukankah dia sudah tiada?..."
+Reina:"hah.. Aku akan menceritakan nya nanti, saat ini kita harus membantu yang lain.."
+
+*Vyerna dan Kaelous menganggukkan kepala mereka*
+Kaelous:"...kita berpencar"
+Vyerna:"Benar aku sempat melihat ada makhluk kegelapan yang juga muncul.."
+Reina:"Benar.. Aku akan pergi ke pilar inti.."
+Vyerna:"Baiklah..."
+Kaelous:"Hati-hati Reina..."
+Reina:"Kalian juga.. Aku mengandalkan kalian!"
+
+*Mereka berpencar dan Reina berlari menyusuri lorong istana yang kini dipenuhi gema alarm sihir dan cahaya merah dari segel pertahanan yang mulai bergetar. Debu melayang di udara, dan bayangan-bayangan hitam bergerak di balik dinding seperti sesuatu sedang mencari celah.*
+
+*Ia menggenggam Dada kiri-nya, merasakan denyut Pilar Inti yang semakin dekan dengan kehadirannya ke kedalaman istana.
+
+Reina:"Zavyre..untuk sekarang aku akan melawan-mu, demi Vulkrath"
+
+*Di sebuah ruang besar berbentuk kubah, Pilar Inti berdiri di tengah, menjulang seperti kristal raksasa yang dipenuhi aliran cahaya biru dan emas. Namun kini, garis-garis hitam seperti retakan mulai merayap di permukaannya.*
+
+*Reina mendekat dan menempelkan telapak tangannya ke penghubung sihir pilar itu*
+
+*Cahaya lembut menjalar dari tangannya,menandakan dirinya sudah bisa memancarkan sihir ke seluruh Vulkrath*
+
+_"Luminara Requiem*_
+
+*Butiran-butiran cahaya melesat dari pilar keluar Istana dan menyebar keseluruh Vulkrath lembut muncul di udara, seperti debu bintang yang tertiup angin malam. Mereka berkilau hangat, tenang, dan indah… namun di balik kelembutannya tersimpan kekuatan pemurnian yang mutlak.*
+
+*Butiran cahaya itu melayang perlahan ke arah Mahluk kegelapan yang menyebar Tidak ada ledakan keras, tidak ada kilatan menyilaukan. Saat mereka menyentuh tubuh makhluk kegelapan, cahaya itu meresap seperti embun, masuk ke dalam bayangan dan energi gelap yang menyelimutinya,dan mahkluk itu langsung hancur di tempat mereka berdiri.
+
+*namun makhluk itu tetap datang dari segala sisi, dari tengah Istana Reina masih bisa mendengar dentingan pedang yang saling menyahut menandakan Ini belum selesai*
+
+Reina:"Semoga ini cukup..setidaknya setiap 20 menit sihirnya akan kembali, aku harus segera pergi keluar.."
+
+*Saat Reina berbalik badan di pintu sudah berdiri sosok berambut putih panjang menghalangi jalan keluarnya*
+
+Astrev:".. Oh kemana kau ingin pergi Putri?.."
+Reina:"Zavy-.. Tidak kau bukan Dia..."
+Astrev:"Hehe.. Tentu saja bukan.."
+
+*Astrev maju beberapa langkah kedepan Reina, dan sebaliknya Reina berjalan mundur perlahan, sampai akhirnya dia menabrak sisi pilar. 
+
+*Astrev menahan  Reina untuk pergi dari ruangan itu*
+
+Astrev:"Tidak perlu terburu-buru putri.. Aku datang kesini untuk mengambil sesuatu dari dirimu.."
+Reina:"Apa yang kau inginkan?!.."
+
+*Astrev Tersenyum sambil mengulurkan tangan nya yang perlahan muncul pedang gelap yang tajam langsung mengarah ke Wajah Reina*
+
+Astrev:"Hidup mu yang abadi itu.."
+Reina:_"Lux Gladius"_, Kita lihat saja"
+
+*Reina memadatkan cahaya menjadi sebuah bilah pedang yang sangat tajam*
+
+*Astrev Tersenyum tajam dan tanpa aba-aba lain, dia langsung maju sambil mengayunkan pedangnya*
+---------------------------------
+*Diluar istana Kaelous dan Vyerna sedang berusaha mengalahkan pasukan Mahkluk kegelapan yang tidak ada habisnya*
+
+Kaelous:_"Tempus Claustra!"_
+*Gerakan Mahluk kegelapan terhenti seolah waktu disekitar Kaelous pun terhenti, dan Vyerna Menyerang mereka yang berhenti dengan lilitan ular yang menghancurkan mereka seketika*
+Vyerna:_"Serpens Oblivio!"_
+
+Kaelous:"Mereka tidak ada habisnya..."
+Vyerna:"Sebentar lagi pagi.. Dan mereka tidak berkurang...aku pikir mereka sudah diperkuat oleh seseorang"
+Kaelous:"aku harap Reina baik-baik saja.. Dia belum terlihat"
+
+*Tiba-tiba ada makhluk yang terlihat lebih besar dari sebelumnya menyerang kearah mereka*
+
+Kaelous:"VYERNA!! _"TEMPUS CHRON-"_
+
+*Tiba-tiba kepala mahkluk itu sudah ditebas dari belakang oleh Lioren yang muncul*
+
+Lioren:"VYERNA,Kaubaik-baik saja!?!"
+Vyerna:"Lyo!.."
+(Kaelous:"Lyo-?") 
+
+*Vyerna menghampiri Lioren dan Lioren langsung memeluknya*
+
+Lioren:"Kau baik-baik saja kan?.."
+Vyerna:"Aku baik-baik saja..."
+(Kaelous:"kau tidak akan menanyakan keadaanku?") 
+
+Vyerna:".. Reina belum kembali dari dalam Ruang pilar sihir... Tapi dia sudah berhasil mengaktifkan sihirnya"
+Lioren:"Aku merasakannya juga sihir itu akan tetap ada setiap 20 menit itu sudah sangat membantu"
+
+Kaelous:"maaf mengganggu pertemuan kalian tapi... Mereka masih tetap dataaaang!!"
+Kaelous:_"Tempus Claustra!"_
+Lioren:"Kaelous! Kau pergi cari Reina Aku dan Vyerna akan mengatasi yang disini!"
+Kaelous:"Baik!!"
 ---------------------------------`,
 };
 
