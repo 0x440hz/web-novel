b3dada80a51e762fcc5c7fe636cde85802efa7fe
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 11:28:38 +0700

    Update ch21.js

--- src/data/novels/novel2/chapters/ch21.js
+++ src/data/novels/novel2/chapters/ch21.js
@@ -1,10 +1,86 @@
 const chapter = {
-  id: 5,
+  id: 21,
   arc: "Arc 2: Flame Arc",
-  title: "#5 Sumpah Dendam di Balik Luka",
+  title: "#21 Perpisahan Tanpa Jawaban",
   content: `
 ---------------------------------
+Zavyre:"Maafkan aku tapi aku akan mengakhiri ini semua dengan caraku"
+Altheas:"... Kau memang Aib keluarga kita Zavyre"
 
+*Altheas mundur beberapa langkah dan akhirnya menghilang dari sana meninggalkan mereka berdua sendirian*
+---------------------------------
+Zavyre:".... Reina"
+Zavyre menghampiri Reina kembali dan melihat seluruh lukanya sudah sembuh dengan sempurna. 
+
+*Dan perlahan Reina, membuka matanya lalu melihat Zavyre sudah duduk di sampingnya*
+
+Reina:"...!!!-KAU?!?"
+Reina hendak bangkit dari posisinya namun dia masih merasakan Sakit yang luar biasa di sekitar punggung dan perutnya. 
+
+Reina:"a... Ahh-.. Apa yang aku lakukan disini.. Dan kenapa kau-.. "
+Zavyre:"... Aku sedang tidak ingin menjawab pertanyaan apapun putri"
+Reina:"?!!.. Kau harus menjawabnya!! Mengapa aku ada disini!! Apa yang kau lakukan padaku?!!"
+Zavyre:".... Kau keras kepala Reina.."
+Zavyre:"aku akan mengantar mu kembali pulang, tapi bukan berarti aku ingin menjadi lebih dekat dengan mu.. Aku masih menyimpan dendam yang sama Reina.. "
+
+*Zavyre berdiri dan mengulurkan tangannya pada Reina berharap dia mau menerimanya*
+
+Reina:"...."
+Zavyre:"ayo.. Aku tahu kau sangat lemah sekarang ini"
+
+*Reina menepis tangan Zavyre dan berdiri dengan kekuatan nya sendiri*
+
+Zavyre:".... Tidak heran kau menolaknya"
+Reina:"Kau akan berjalan di Depanku.. "
+Zavyre:".. Hah.. Sesuai keinginanmu"
+
+*Mereka berdua, mulai berjalan untuk melewati hutan menuju Vulkrath*
+*Diperjalanan mereka berdua hanya saling diam, namun Reina tetap waspada pada zavyre*
+---------------------------------
+*Kabut tipis menyelimuti jalur hutan Vulkrath.Zavyre berjalan di depan, sementara Reina mengikutinya beberapa langkah di belakang. Jarak itu kecil… tapi maknanya besar.*
+
+Reina:"Kalau kau masih berencana untuk membalas dendam mu padaku… ini saat yang paling Tepat."
+Zavyre:"Aku tidak akan membunuh seseorang yang lebih lemah dari ku.. Itu sama sekali tidak menantang"
+Reina:"... Tch.. "
+
+*Hutan semakin gelap. Akar-akar besar melintang di tanah, dan Reina hampir tersandung. Tanpa menoleh, Zavyre memperlambat langkahnya.*
+
+Zavyre:
+"Perhatikan langkahmu."
+
+*Reina terdiam sesaat.*
+
+Reina:""… Kau memperhatikanku?"
+Zavyre:
+"Aku memperhatikan jalan yang kita lalui dan aku kebetulan saja melihat batu yang kau injak tadi."
+Reina:"... Kau menyebalkan"
+
+*Mereka terus menelusuri Hutan yang luas itu, selangkah demi selangkah mereka lalui tanpa banyak berbicara satu sama lain*
+
+*dan tidak terasa mereka sudah sampai di perbatasan hutan dan juga desa Vulkrath*
+
+Zavyre:".... Cukup disini saja benar"
+Reina:"aku rasa.. Iya"
+
+*Zavyre berbalik badan dan langsung berjalan untuk pergi meninggalkan Reina*
+
+*Reina terdiam ditempat nya tidak percaya bahwa dia baru saja menghabiskan sepertiga malam berjalan bersama seseorang yang sangat dia benci*
+
+Reina:"..... Zavyre.. "
+Suara Reina begitu pelan, hingga Zavyre tidak mendengarnya sama sekali. 
+
+Reina:_"Kau menyelamatkanku… walau kau membenciku ya..."_
+---------------------------------
+*Reina berjalan memasuki Desa Vulkrath. Melewati setiap jalan yang sepi karena malam itu semua orang sudah tertidur hanya ada beberapa prajurit yang berjaga.*
+
+Prajurit 1:"PUTRI REINA!"
+Reina:".... Hehe "
+Prajurit 2:"AKHIRNYA KAU KEMBALI KAPTEN KRYLE SANGAT MENGKHAWATIRKAN MU PUTRI"
+Reina:"maafkan aku.. Aku pergi terlalu lama"
+Prajurit 1:"Kami akan mengantar mu kembali ke istana!"
+Reina:"Terimakasih banyak"
+
+_"SOLAN!"_
 ---------------------------------`,
 };
 
