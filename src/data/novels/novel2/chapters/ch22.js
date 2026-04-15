84372f960f60c664a8c8239c7b9af0031a40fbcb
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 11:31:42 +0700

    Update ch22.js

--- src/data/novels/novel2/chapters/ch22.js
+++ src/data/novels/novel2/chapters/ch22.js
@@ -1,10 +1,108 @@
 const chapter = {
-  id: 5,
+  id: 22,
   arc: "Arc 2: Flame Arc",
-  title: "#5 Sumpah Dendam di Balik Luka",
+  title: "#22 Perasaan yang Salah Waktu",
   content: `
 ---------------------------------
+_"Walaupun jika kehidupan sebelumnya memang ada,aku tetaplah Reina...... Reina.... Kenapa semua orang memanggil ku Reina?"_
 
+"Reina... Reina... Bangun.."
+
+Reina:"Suara siapa itu......"
+
+"Semua Dosa-dosa mu tidak Terampuni...Reina"
+---------------------------------
+Kryle:"Reina... Apa kau sudah bangun?"
+*Kryle mengetuk pintu kamar Reina*
+
+Reina:"..... Hah.. Aku sudah bangun..."
+
+*Kryle langsung membuka pintu dan masuk ke dalam kamar Reina sembari membawa sarapan untuk Reina*
+
+Kryle:"Bagaimana keadaan mu Reina?"
+Reina:".... Aku.. Bingung kryle"
+Kryle:"ada apa?... "
+Reina:"Sebenarnya... "
+
+*Reina menceritakan apa yang sebenarnya telah terjadi padanya tempo hari, dan disaat itu wajah kryle langsung murung tampak Kecewa, Marah, dan Takut menjadi satu di wajahnya*
+
+Kryle:"... Begitu.."
+Kryle:"Reina..kau yakin kau baik-baik saja? 
+*Reina hanya menganggukkan kepalanya*
+
+Kryle:"Abadi.... Ya?.."
+Reina:"... Aku.. Tidak tahu apa yang harus aku katakan lagi padamu kryle"
+Kryle:"...tidak apa-apa Reina.. Hal ini memang sangat berat untuk di mengerti..bahkan untuk dirimu sendiri"
+Reina:"ya... Kurasa kau benar"
+Kryle:"sudahlah.. Lihat aku membawakan mu sarapan, sebaiknya kau segera menghabiskannya "
+Reina:"hehe... Terimakasih kryle.. "
+
+*Reina pun memakan sarapan yang di bawakan oleh kryle, dan untuk beberapa saat mereka mengobrol berdua di dalam kamar itu*
+
+*Untuk sementara itu Kryle merasa jika hal itu adalah sesuatu yang sangat berharga bagi kryle, itu bukan hanya sekedar menghabiskan waktu berdua dengan Reina, tetapi lebih dari itu... Sampai tidak bisa di artikan dengan kata-kata sederhana saja*
+
+*Tiba-tiba saat mereka sedang mengobrol, Kryle langsung menggenggam tangan Reina, dia membuat suasana hening seketika, Reina menatap Kryle dengan kebingungan*
+
+Reina:"ada sesuatu?"
+Kryle menutup matanya sebentar dan menarik napas panjang sebelum akhirnya menghembuskan nafasnya.. 
+
+Kryle:"Reina... "
+Dia menggenggam kedua tangan Reina dan mengangkatnya sedikit lebih tinggi diantara mereka berdua
+
+Kryle:"Reina sebenarnya aku ingin  mengatakan hal ini sejak lama...Apa kau bersedia untuk mendengarkan ku? "
+Reina:"...Tentu Kryle... "
+
+Kryle:"Reina... Tidak peduli, seberapa rapuhnya dirimu, tidak peduli semua yang orang-orang katakan tentangmu, tidak peduli dengan segala kekuranganmu.. Reina... Aku menyukai dirimu... 
+Reina:"..... HA-"
+Kryle hanya terdiam dengan wajah dan telinganya yang menjadi merah karena malu. 
+
+Reina:"... A.. Aku juga menyukai-"
+Kryle:"TIDAK REINA!! ... Ini bukan perasaan menyukai yang seperti kau kira Reina... AKU MENCINTAIMU!!! BUKAN SEBAGAI RAKYAT VULKRATH BUKAN JUGA SEBAGAI PEMIMPIN PRAJURIT KERAJAAN!! AKU MENCINTAIMU SEBAGAI SESEORANG YANG BENAR-BENAR PEDULI PADAMU!!! 
+Reina:"E-EHHH?!?!"
+Kryle:"Dan jika aku diberi kesempatan untuk memiliki dirimu sepenuhnya.. Aku ingin kau juga mencintai ku dengan ABADI!!! 
+
+*Kamar itu menjadi sangat sunyi.*
+
+*Hanya terdengar napas mereka berdua yang sedikit tidak teratur. Tangan Kryle masih menggenggam tangan Reina, erat… namun gemetar.*
+
+*Wajah Kryle benar-benar merah, tetapi matanya tetap menatap Reina dengan serius tanpa ragu sedikit pun.*
+
+*Hingga muncul suara ketukan pintu yang memecah keheningan itu*
+
+Prajurit:"Putri Reina... Apa Kapten Kryle ada di dalam?*
+Kryle lalu melepas genggaman tangannya dari reina dan langsung berdiri setelah mendengar suara itu
+
+Kryle:"Aku di dalam sini..."
+Prajurit:"Laporan kerajaan bulan ini kapten"
+
+*Kryle melihat ke arah Reina dan mengusap-ngusap wajahnya agar terlihat seperti tidak ada yang terjadi saat berhadapan dengan prajurit itu*
+
+Kryle:"Aku... Akan segera kembali.."
+
+Prajurit:"Meminta izin untuk membuka pintu mu putri Reina"
+Reina:"ku Izinkan"
+
+*Prajurit itu membukakan pintunya untuk kryle, Sebelum berjalan keluar, Kryle berhenti di ambang pintu. Ia menoleh sekali lagi.*
+
+Kryle:"... Selamat Beristirahat Putri"
+
+*Lalu mereka berdua akhirnya pergi dan meninggalkan pintu yang tertutup di hadapan Reina.*
+
+*Reina hanya terdiam dengan apa yang baru saja terjadi*
+
+*Reina duduk kembali di tepi ranjang,Kamar itu terasa jauh lebih sepi dari sebelumnya.*
+
+*Ia menatap jendela, cahaya pagi menyinari wajahnya. Lalu… tanpa sadar… pikirannya kembali pada satu sosok.*
+
+_Zavyre._
+
+*Reina meletakkan tangannya di dada.*
+
+Reina:"Kenapa… saat kryle berkata seperti itu… malah wajahmu yang muncul di benak-ku?"
+
+*Angin lembut berhembus masuk melalui jendela. Tirai putih bergoyang perlahan. Dan di kejauhan di balik hutan Vulkrath,Zavyre berdiri sendirian, menatap ke arah istana. Seolah ia bisa merasakan sesuatu telah berubah.*
+
+_Zavyre:"Maafkan aku Tuan Putri, tapi sebenarnya aku sudah mengesampingkan tujuan awal ku sejak lama, ketulusan...mungkin itu yang ada dalam diriku saat ini"_
 ---------------------------------`,
 };
 
