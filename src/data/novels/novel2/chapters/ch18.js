fb3310abf695e1be05f8b3db241894ee7b1d28df
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 07:12:39 +0700

    Update chapter title and content in ch18.js

--- src/data/novels/novel2/chapters/ch18.js
+++ src/data/novels/novel2/chapters/ch18.js
@@ -1,10 +1,113 @@
 const chapter = {
   id: 18,
   arc: "Arc 2: Flame Arc",
-  title: "#18 Sumpah Dendam di Balik Luka",
+  title: "#18 Perasaan yang Dibatalkan di Ujung klimaks",
   content: `
 ---------------------------------
+*Di sisi lain Kryle masih menunggu Reina yang belum kunjung datang ke tempat latihan berpedang para prajurit istana sembari masih memikirkan kata-kata yang hampir terucap dari mulut nya di hadapan Reina.*
 
+*Kryle tak bisa menahan Wajahnya yang menjadi merah untuk waktu yang lama sampai akhirnya ada yang menegur dirinya*
+
+Prajurit:"Kapten Kryle... ?"
+
+Prajurit itu menepuk pundak kryle dari belakang yang mengirimkan perasaan kejut pada tubuh kryle.
+
+Kryle:"WAH!.. Oh-... Apa?"
+Prajurit:"apa kau baik-baik saja?"
+Kryle:"hm? Ya.. Ya aku baik-baik "
+
+Terdapat sedikit keraguan dari nada bicara kryle yang langsung membuat prajurit itu menyadari ada sesuatu yang dia sembunyikan. 
+
+Prajurit:"... Oh.. Benarkah Begitu kapten?"
+Kryle:"Ya.. Jangan khawatir.. Eee... Siapa namamu?-"
+
+_"SOLAN!,Aku Lioren dari divisi keamanan Penduduk bagian barat!"_
+
+Kryle:"Lioren.. Ah aku ingat sekarang.. Terimakasih karna sudah peduli dengan keadaan ku Lioren.."
+Lioren:"Sudah menjadi tugas ku untuk memastikan kebutuhan Kerajaan dan Kapten prajurit kita terpenuhi"
+
+Nada bicara Lioren sangat tegas namun juga terasa ringan dan hal itu cukup membuat keraguan Kryle sedikit berkurang. 
+
+Lioren:"Kapten.. Apa kau menunggu sesuatu?..karna tidak seperti biasanya kau hanya berdiam diri di pojok seperti ini jika berada di tempat berpedang.."
+Kryle:"Ya aku menunggu Rei-.. Akhem.. Maksudku Putri Reina untuk kemari.."
+Lioren:"APA!??!!!!!!!!"
+
+Suara lantang Lioren mengejutkan kryle lagi dan membuat prajurit lain yang ada di sana ikut menoleh kearah mereka. 
+
+Kryle:"-.. P-pelankan suara mu-"
+Lioren:"KAPTEN!! APA ITU BENAR APA PUT-"
+
+Kryle langsung menjepit mulut Lioren agar tidak melanjutkan kata-kata nya. 
+
+Kryle:"Kubilang pelankan suaramu"
+Lioren:"SOLAN!!"
+Kryle:"hah.... "
+
+Kryle melepaskan jepitan tangannya dari mulut Lioren. 
+
+Lioren:"apa itu benar putri akan kesini?.. Aku sangat bersemangat setelah mendengar itu.. Karna ini akan menjadi kesempatan ku untuk melihat putri Reina secara langsung!! Dan.. Di Hadapan ku!!"
+
+Lioren berhenti sejenak dengan memasang raut wajah kebingungan. 
+
+Lioren:"tapi.... Ada perlu apa dia datang kesini kapten?"
+Kryle:"ah.. Aku hanya mengajaknya untuk latihan berpedang biasa saja"
+Lioren:"Wooooohh!! Aku akan melihat gaya berpedang putri Reina Secara langsung!!!!"
+
+Kryle tersenyum beberapa saat pada reaksi Lioren yang sangat bersemangat namun kenyataan bahwa Reina belum datang langsung kembali menghampiri pikirannya. 
+
+Kryle:("Kenapa dia belum datang juga..?") 
+---------------------------------
+*Kembali ke Kamar Reina*
+
+*Setelah menikmati beberapa langkah dansa mengelilingi kamar Reina, Zavyre menghentikan Langkah nya namun tangan Zavyre tetap menggenggam tangan Reina dan menahan pinggang Reina dengan tangannya yang lain.*
+
+Zavyre:"lucu sekali.. Semalam kita saling beradu Sihir yang sangat Dasyat.."
+Zavyre:"Namun saat ini kau terjebak di genggaman ku dan mengikuti setiap langkah ku tanpa paksaan sedikit pun"
+Reina:"... Aku hanya mengikuti alur untuk mengetahui alasan mu sebenarnya"
+
+Zavyre tersenyum kecil padanya
+
+Zavyre:"Aku sudah mengatakan itu sebelumnya.. Aku datang kesini untuk melihat keadaan mu saja putri~"
+Reina:"Kau memang Sangat menyebalkan.. "
+Zavyre:"hehe... Apa benar seperti itu?.. "
+Zavyre  mendekatkan wajahnya dengan Reina dan sekali lagi menarik pinggang Reina hingga menempel dengan pinggang Zavyre. 
+
+Zavyre:"Tak lama lagi Kau akan Tau apa itu kehilangan yang sebenarnya..Reina"
+Reina:"MUNDUR!!"
+Reina berusaha membuat jarak di antara mereka berdua. 
+
+*namun bukannya Zavyre memundurkan dirinya..dia justru makin mendekatkan wajahnya dan Reina hingga Kening dan Hidung mereka Saling bersentuhan.*
+
+*Hanya satu tarikan napas memisahkan bibir mereka.*
+
+*Reina bisa melihatnya dengan jelas sekarang tidak ada niat membabi buta. Yang ada hanyalah keinginan yang ditahan dengan sengaja.*
+
+Zavyre:"Andai aku mengikuti dorongan hatiku Reina.."
+
+*Waktu seakan melambat saat  Zavyre tiba-tiba menutup matanya dan hendak menyatukan bibirnya dengan Reina.*
+---------------------------------
+_* Klang *_
+Suara logam bergetar.
+
+*Pedang Reina yang sebelumnya jatuh bergeser sedikit di lantai, tersentuh ujung sepatu Zavyre saat ia melangkah setengah jengkal lebih dekat.*
+
+*Bunyi kecil itu… cukup untuk membuat Zavyre kembali dari pikirannya kabur dan membeku.*
+
+*Mata Zavyre Terbuka, seolah ia baru saja menyadari batas yang hampir ia langgar sendiri.*
+
+*Perlahan..sangat perlahan..ia menarik kepalanya mundur dan tangannya melepaskan semua sentuhan dari Reina.*
+
+Zavyre:"Bukan hari ini, Putri Reina."
+Ia melangkah mundur satu langkah. Lalu dua..jarak mulai tercipta diantara mereka berdua. 
+
+Zavyre:"... Tujuan ku kesini sudah terpenuhi...... Sampai jumpa,Putri...atau tidak"
+Zavyre menghilangkan dirinya diantara bayangan seperti biasa. 
+Meninggalkan ruangan yang kembali sunyi. 
+
+*Reina berdiri kaku beberapa detik sebelum akhirnya menghembuskan napas panjang.*
+
+Reina:"... Orang itu.. Berbahaya.. "
+Reina kembali ke dalam pikirannya.. Dan mulai membelai bibirnya dengan jari. 
 ---------------------------------`,
 };
 
