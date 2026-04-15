e166cce576f1ee78aa65e96fa70948ad35d72710
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 11:50:27 +0700

    Update ch26.js

--- src/data/novels/novel2/chapters/ch26.js
+++ src/data/novels/novel2/chapters/ch26.js
@@ -1,10 +1,152 @@
 const chapter = {
   id: 26,
   arc: "Arc 2: Flame Arc",
-  title: "#26 Sumpah Dendam di Balik Luka",
+  title: "#26 Pertemuan Setelah Kehilangan",
   content: `
 ---------------------------------
+_Langit kembali biru._
+_Scutum Lucis masih berdenyut pelan._
+_Medan perang sunyi._
 
+*Reina melayang sendirian di udara, dan perlahan turun ke tanah untuk melihat sekeliling*
+
+*Reina berlutut.*
+
+*Akhirnya Air mata jatuh ke tanah yang hangus.*
+
+_Bukan sebagai putri._
+_Bukan sebagai Ksatria._
+_Tetapi sebagai manusia yang kehilangan orang yang dicintainya._
+---------------------------------
+*Di suatu Tempat lain yang tidak diketahui keberadaan nya, didalam sebuah ruangan yang terlihat seperti perpustakaan seorang pria berdiri diantara rak-rak buku tua*
+
+*telinganya berdenging,seakan menandakan sesuatu telah terjadi*
+
+_"Sudah selesai ya"_
+
+*pria itu memiliki postur tubuh tinggi, memiliki rambut merah, dan warna mata yang serasi dengannya*
+
+*Pria itu menghampiri sebuah sudut perpustakaan dan melihat kearah Figura Foto yang memiliki beberapa orang didalamnya*
+
+_"Kau sudah sangat kuat..... Reina,aku harap kau sudah menerima salam hangat ku untuk mu"_
+---------------------------------
+*beberapa bulan setelahnya,kerajaan Vulkrath masih sama, hilangnya kedua orang yang di cintai membuat perbedaan kecil yang memiliki dampak besar*
+---------------------------------
+*Suara ketukan pintu kamar menggema di kamar Reina yang terasa kosong*
+Lioren:"Putri Reina...."
+Vyerna:".. Reina..kau didalam?"
+
+Lioren:"Aku rasa dia masih.. "
+Vyerna:"Sst! Tidak di depan kamarnya"
+Lioren:"Maaf-"
+
+*Tiba-tiba pintu terbuka perlahan dan sosok Reina muncul untuk pertama kalinya didepan orang lain dalam beberapa bulan*
+
+Lioren:"Putri Reina-!"
+Vyerna:"... -"
+Reina:"Ada apa...."
+Lioren:"Putri Reina, Kami kemari untuk mengecek keadaan mu seperti biasa dan... Dan.. -"
+Vyerna:"Dan kami rasa sudah terlalu lama kau berada didalam sana.. Reina"
+Reina:"Terimakasih sudah mengkhawatirkan diriku, apa ada hal lain yang ingin kalian katakan?"
+Lioren:"Y-Ya putri.. Um, salah satu prajurit kita yang berjaga di gerbang istana, dihampiri seorang pria yang mengaku adalah teman lama dari mu"
+
+Reina:"hm??.... "
+Lioren:"dan dia menitipkan pesan bahwa dia ingin bertemu dengan mu di pusat desa"
+Vyerna:"Saran ku Reina, kau tidak perlu terlalu menggubris hal itu-..mengingat... Kau.. Masih.. Um"
+
+Reina:"... tidak ada pilihan lain,aku harus melakukannya..aku tidak suka membuat orang lain menunggu.."
+Lioren:"K-kami akan ikut dengan-Mu"
+Reina:"Tidak perlu"
+Lioren:"... Sesuai perintahmu"
+
+Vyerna:"hah...Reina.. Kau yakin ingin keluar?"
+*Reina hanya membalas pertanyaan itu dengan menganggukkan kepalanya*
+
+*Reina bersiap untuk pergi..saat dia sudah siap untuk berangkat dia menyelusuri lorong istana,dan saat ia akhirnya tiba di gerbang Lioren menahannya sebentar lalu mengajukan pertanyaan terakhir*
+
+Lioren:"Putri Reina, kau yakin tidak perlu didampingi?"
+Reina:"tidak perlu.. "
+
+*Lioren membungkuk lalu mengarahkan Prajurit untuk membukakan gerbang*
+
+Lioren:"Hati-hati..Putri Reina"
+
+*Reina melangkah keluar istana dan meninggalkan Lioren yang memasang wajah cemas sampai akhirnya gerbang kembali tertutup*
+---------------------------------
+*Di desa, semua penduduk dan beberapa prajurit masih aktif melakukan hal mereka masing-masing, dan dengan terpaksa Reina menyembunyikan kesedihannya untuk sementara waktu*
+
+Prajurit:"selamat pagi putri Reina"
+*Reina tersenyum dan hanya mengangguk*
+
+*Setibanya Reina di Pusat Desa, tepatnya di sebuah air mancur Tua yang menjadi titik pusat desa Vulkrath, dia melihat seorang laki-laki yang berdiri membelakangi nya*
+
+*pria dengan rambut merah, berbadan tinggi dan aura familiar yang sangat kuat*
+
+???:"Kau datang juga ya... Reina~"
+Reina:"..?"
+
+*Pria itu berbalik, dan akhirnya Reina bisa melihat siapa orang itu*
+
+Reina:"Kael..Ous?"
+Kaelous:"... Oh.. Kau mengingat diriku?"
+
+*Reina merasa bingung bagaimana dia bisa menyebut nama itu, yang jelas seingat dia itu adalah pertama kalinya mereka bertemu*
+
+Reina:"Maafkan aku, hanya saja mulutku tiba-tiba menyebut nama itu, itu bukan namamu...... B-benar kan?"
+Kaelous:"hehe.. Itu namaku"
+
+*Reina terdiam*
+Reina:"tapi bagaimana bisa-"
+Kaelous:"tebakan beruntung?"
+Reina:"...."
+Kaelous:"Lama tidak bertemu.."
+
+*Kaelous menghampiri Reina, dan langsung menjentikkan jarinya di dahi Reina,Sentuhan itu tiba-tiba membuat Reina seakan sadar akan sesuatu*
+
+Kaelous:"Bagaimana.. Sudah jelas?"
+Reina:".... Kaelous"
+
+*Kaelous langsung memeluk Reina dengan Erat, dan Reina pun membalas pelukan itu*
+
+Kaelous:"Akhinya aku menemukanmu Reina.."
+Reina:"Kau sungguh mencari ku?"
+Kaelous:"tentu saja!.. Aku mengaitkan semua sejarah yang ada.. Sampai akhirnya aku sampai disini"
+
+Reina:"kaelous.... Ka.. Elous"
+Kaelous:"Ya Reina?"
+
+*Tiba-tiba saja kaelous merasakan sesuatu pada Reina*
+
+Kaelous:"apa kau menangis?.."
+
+*Benar saja Reina menangis di pelukannya, dan tidak sanggup untuk mengatakan apapun*
+
+*kaelous memeluknya lebih erat dan menaruh salah satu tangannya di kepala Reina*
+
+_Kaelous:"kau memang kuat Reina, tapi ku boleh menangis sebanyak yang kau mau"_
+---------------------------------
+*Pagi itu untuk pertama kalinya Kaelous melihat Reina menangis di hadapannya, dan akhirnya mereka memutuskan untuk duduk di tepi air mancur untuk beberapa waktu*
+
+Kaelous:"apa kau sudah tenang Reina?"
+Reina:"... Ya... T-terima kasih"
+Kaelous:"hah.... Bagaimana rasanya menjadi putri sebuah kerajaan Reina?"
+Reina:"sangat berat.. Aku rasa menjadi putri lebih berat daripada yang terlihat"
+Kaelous:"aku mengerti"
+Reina:"Aku kagum pada dirimu Kaelous.. Kau sangat luar biasa.. Bisa menemukanku disini, apalagi setelah bertahun-tahun lamanya"
+Kaelous:"ini berkatmu juga Reina.. Jika kau tidak memberikanku sebagian sihir mu, aku tidak akan ada disini sekarang"
+
+Reina:"benar juga.... Apa berarti kau juga abadi?"
+Kaelous:"Hehe, sayangnya tidak, aku masih bisa mati kapan saja..seperti nya perjalanan waktu membuatku masih terlihat muda"
+Reina:".. Begitu"
+
+*Kaelous memegang tangan Reina, gerakan itu membuat perasaan Familiar kembali, tetapi kali ini oleh Kaelous, bukan Kry-*
+
+Reina:"T-tidak"
+*Reina melepaskan tangannya dari genggaman kaelous*
+
+Kaelous:"... Reina aku ingin membantu mu sembuh "
+Reina:"T.. Tidak sekarang"
+Kaelous:"tidak sekarang, tapi segera "
 ---------------------------------`,
 };
 
