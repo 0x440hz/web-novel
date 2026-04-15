e5071a69e54ab96b8ad3c612afa4edc898b59ec5
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 07:18:04 +0700

    Update ch20.js

--- src/data/novels/novel2/chapters/ch20.js
+++ src/data/novels/novel2/chapters/ch20.js
@@ -1,10 +1,156 @@
 const chapter = {
   id: 20,
   arc: "Arc 2: Flame Arc",
-  title: "#20 Sumpah Dendam di Balik Luka",
+  title: "#20 Darah, Dosa, dan Kehidupan tanpa akhir",
   content: `
 ---------------------------------
+*Beberapa hari setelah Kunjungan Zavyre dan latihan pedang di istana, tidak seperti biasanya di hari itu Reina mengunjungi pemakaman yang ada di ujung desa sendirian.*
 
+*Reina hanya memandangi makam-makam yang ada di sana dan tentu saja makam kedua orang tuanya*
+
+Reina:"... Ibu.. Ayah.... Ntah mengapa aku merasakan sesuatu yang tidak asing muncul di Vulkrath.. Aku harap bukan hal yang lebih buruk dari Zavyre"
+
+???:"Benar.. Semoga tidak lebih buruk dari Zavyre.. "
+Reina:"....?!"
+
+*Suara seorang wanita yang asing tiba-tiba menyahut dirinya dari belakang.*
+
+Reina:"siapa....?"
+Altheas:"aku Altheas.."
+Reina:"..... Kau memiliki aura Sihir yang sama dengan Zavyre"
+Altheas:"wah... Ternyata putri Vulkrath memang hebat.."
+Altheas:"Tidak heran.. Keponakan ku yang lugu itu begitu tertarik padamu..."
+Reina:"Keponakan?!!... "
+Altheas:"Hehe.. Benar Zavyre adalah keponakan ku.. "
+
+*seketika Reina merasakan ada sesuatu yang menariknya untuk terus melihat ke arah Wanita itu, walaupun beberapa kali dia menolak tapi kekuatan itu seakan memaksanya untuk tetap mendaratkan pandangannya pada Altheas*
+
+Altheas:"Ada apa putri Vulkrath.. Tidak bisa berpaling karena kecantikan ku ya~?"
+Reina:" ... "
+Altheas:"tak ku sangka pertemuan Pertama kita harus berakhir dengan sangat cepat"
+
+*Reina hanya membeku ditempatnya tidak bisa kemana-mana, dia ingin melawan.. Namun tidak ada gunanya.. Ia tidak bisa melakukan apa-apa*
+
+Altheas:"Rekan ku memberitahu diriku bahwa putri Vulkrath itu Abadi.. Dan aku tidak akan percaya hingga aku benar-benar melihatnya dengan mataku sendiri.."
+Altheas:"Beritahu aku apa saja yang ada di kehidupan setelah kematian Reina"
+Altheas:"_Vitreus Umbra_"
+pecahan kaca muncul dari bayangan Reina lalu menusuk punggungnya dari belakang. 
+---------------------------------
+*Di saat yang bersamaan Di tempat persembunyian Zavyre.*
+
+Zavyre:"... Ahhhkk.. Apa ini-.. Telingaku tiba-tiba berdenging"
+Vyerna:"Zavyre?!.. Ada apa? "
+Vyerna:"!!!.. Reina?!"
+
+*Secara tiba-tiba saja Vyerna merasakan kekuatan Reina di sekitar mereka*
+*Vyerna mencari ke sekeliling tapi tidak ada tanda-tanda Reina sama sekali yang ada hanyalah..*
+
+Altheas:"kau Mencari ku wanita ular?"
+Mata Vyerna seketika terbelalak dan seluruh tubuhnya membeku ketika mendengar suara itu.
+
+*disisi lain Zavyre yang masih kesakitan tetap meronta-ronta di dalam gua*
+
+Altheas:"Zavyre... Itukah dirimu?.. Apa yang terjadi padamu?.."
+Vyerna:".......!!!"
+Vyerna melihat ke arah altheas yang ternyata dia membawa tubuh seseorang yang sangat mereka kenal
+
+Vyerna:"R... Reina!?!"
+Zavyre:"Reina... BUAT DENGINGAN INI BERHENTI!!!!!"
+Altheas:"..... Aku mengerti, sekarang.. Zavyre, aku tidak menyangka kau melakukan hal yang paling aku benci"
+Vyerna:"Zavyre?..... Apa yang kau telah lakukan!?!!"
+Zavyre:"BAIK AKU AKAN MENGAKUI NYA!!! TAPI.. AHHHKKKK!!!"
+
+Altheas:_"Tenebrae anima, vulnus devora… vitam redde."_
+
+*Perlahan Zavyre merasakan dengingan suara yang ada di telinganya menghilang, dan Zavyre merasa lebih tenang*
+
+Zavyre:" ........ "
+Vyerna:"Zavyre....?"
+
+*Zavyre berdiri dan terdiam beberapa saat dan tiba-tiba dengan gerakan yang sangat cepat ia mencekik leher altheas dan mendorong nya ke dinding*
+
+Zavyre:"Kau pikir apa yang sedang kau lakukan!?!!"
+Altheas:"Ck.. S-seharusnya Kau yang disalahkan atas perbuatan mu!!"
+Zavyre:_"DARC BASTILLE"_
+Kurungan yang terbuat dari bayangan akhirnya dapat mengurung Altheas dan membuat nya tidak bisa berkutik.
+
+Altheas:"DASAR BODOH!KAU PIKIR APA YANG TELAH KAU  LAKUKAN DENGAN PUTRI ITU!?!!!!"
+Zavyre:"DIAM INI URUSANKU!!!"
+Altheas:"Kau sudah melenceng sangat jauh dari tujuan awal keluarga kita Zavyre.. SANGAT JAUH!!!!"
+Altheas:"Kurang ajar, kau menaruh kehidupan mu di dalam diri putri itu"
+
+Vyerna:".. Zavyre......."
+Zavyre:"diam Vyerna... Pergilah"
+Vyerna:"aku pikir kau sudah membuat keputusan.... "
+Zavyre:"Tentu.. Dan inilah keputusan ku"
+
+Altheas:"...Perasaan mu adalah penghambat Zavyre"
+Vyerna:".... Kau sangat kelewatan Zavyre.. "
+
+*Zavyre tidak mempedulikan perkataan mereka dia hanya berjalan menuju tubuh Reina yang terbaring lemas di depannya*
+---------------------------------
+*Reina terbangun ditempat asing.*
+
+*Ia berdiri dan berada di sebuah ruang luas tanpa langit dan tanah,Segalanya berwarna putih terang dan sangat menyilaukan mata*
+
+*Di kejauhan, ribuan bayangan berjalan perlahan..jiwa-jiwa yang tidak sepenuhnya mati, tapi juga tidak hidup.*
+
+*Reina menatap kedua tangannya.*
+
+*Mereka transparan*
+
+Reina:"... Jadi inikah..kematian?"
+
+???:"Bukan..Ini hanyalah penghubung antara Kehidupan dan kematian"
+
+*Sosok bercahaya muncul di hadapannya yang sangat mirip Reina, tapi matanya seperti galaksi yang berputar.*
+
+???:Tempat mu bukan disini Reina.. Sudah ke berapa kalinya kau kembali kesini.. 
+Reina:"apa maksud mu itu?.."
+???:"Dosa mu sebelum Tidak bisa di bayarkan begitu saja..."
+???:"Ingatlah satu hal..."
+
+_"Keabadian mu Adalah kutukan dan hukuman paling pahit yang bisa kau Terima, sebagai balasan dosa-dosa mu"_
+
+*seketika semua menjadi sangat Terang, hingga membutakan penglihatan Reina.*
+---------------------------------
+*Zavyre Terduduk diam Di samping Reina yang masih terbaring lemah*
+
+*Hanya ada keheningan yang ada diantar mereka Bertiga*
+
+Vyerna:"... Zavyre.. Kau sangat egois...."
+Vyerna pergi meninggalkan Gua meninggalkan Zavyre dengan Altheas. 
+
+Altheas:"..... Kau tidak akan mendapatkan apapun atas tindakan gegabah mu ini Zavyre.. "
+
+*lagi-lagi Zavyre hanya terdiam memperhatikan Reina sambil sesekali memainkan beberapa helai rambutnya*
+
+*Tidak lama kemudian Tiba-tiba saja luka-luka yang ada di tubuh Reina bersinar dengan diikuti oleh tertutup nya kembali luka-luka itu*
+
+Zavyre:"........ "
+Altheas:"Jadi .. Dia benar-benar tidak bisa mati"
+Zavyre:"Apa maksud mu?... "
+
+Altheas:"Kaelous... Si penjaga perpustakaan itu menyelidiki kehidupan Reina.."
+Zavyre:"Kaelous... Pria merah itu... "
+Altheas:"benar... Ntah mengapa dia juga sangat tertarik pada Reina, sampai-sampai dia menyimpulkan bahwa Reina itu abadi..ntah bagaimana seperti nya teori Kaelous tentang Reina benar.."
+Zavyre:"... Dia abadi...? -"
+Altheas:"Benar.. Aku datang kesini untuk membuktikannya dan ternyata.. Itu benar"
+
+*Altheas terdiam beberapa saat dan mulai tertawa.*
+
+Zavyre:"apa yang begitu lucu dari hal itu? "
+Altheas:" Oh~ Zavyre keponakan ku yang begitu lugu.. "
+
+Altheas:_"Arcafragor"_
+Kurungan sihir Zavyre tiba-tiba Hancur dan Altheas melangkah keluar darinya. 
+
+Zavyre:"KAU!"
+Altheas:"kau benar-benar bodoh Zavyre.. Berikan tubuh Reina itu padaku dan kita akan mendapatkan keabadian yang dia miliki!"
+
+*Zavyre menjadi ragu untuk sesaat dan akhirnya dia berdiri di hadapan Altheas, mencoba untuk menghalangi nya dari Reina*
+
+Altheas:"...... Kau memang payah Zavyre"
 ---------------------------------`,
 };
 
