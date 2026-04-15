b65dcd4a35ce64508c9772f2a0666d2a12d05030
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 11:33:51 +0700

    Update ch23.js

--- src/data/novels/novel2/chapters/ch23.js
+++ src/data/novels/novel2/chapters/ch23.js
@@ -1,10 +1,138 @@
 const chapter = {
-  id: 5,
+  id: 23,
   arc: "Arc 2: Flame Arc",
-  title: "#5 Sumpah Dendam di Balik Luka",
+  title: "#23 Sumpah di Bawah Cahaya Perlindungan",
   content: `
 ---------------------------------
+_Zavyre:".... Reina jika sesuatu yang besar terjadi.. Aku akan mencoba untuk melindungi dirimu dan juga mendapatkan kepercayaan mu sepenuhnya padaku"_
 
+_Kryle:"Reina.. Tidak peduli kekurangan yang kau miliki, aku berjanji untuk terus bersamamu sampai titik darah terakhir ku"_
+
+_Reina:".... Sesuatu akan terjadi.. Aku harap Vulkrath aman"_
+---------------------------------
+*Pagi itu Semua orang sedang melakukan kegiatan mereka masing-masing, warga desa, prajurit, dan Kryle juga sudah mulai bekerja untuk hari itu*
+
+*Hanya Reina yang Berdiam diri di balkon kamarnya di dalam istana melihat semua kegiatan di desa dan istana dari ketinggian dan... Melihat hutan yang sunyi*
+
+Reina:"Tidak...Dia tidak pantas untuk itu"
+Reina:"Dia hanya menggodaku saja.. Tidak lebih"
+
+*Disaat Reina sedang terjebak dalam pikirannya, tiba-tiba tampak di kejauhan muncul sebuah garis Hitam berpadu dengan warna merah yang menyala yang terlihat seperti Suar gelap menembus langit*
+
+Reina:".... Apa itu... "
+
+*Dan dalam sekejap juga Suar misterius itu menghilang, seakan itu adalah pertanda sesuatu yang besar akan muncul*
+
+*Belum sempat Reina mengetahui sumber dari suar hitam itu tiba-tiba saja sesuatu datang dengan Cepat kearahnya, masuk kedalam kamar dan menghantam dinding dengan keras*
+
+Reina:"..!!!!"
+
+*Debu dari hantaman itu menyebar di seluruh kamar Reina dan dia mencoba melihat apa yang baru saja masuk*
+
+Reina:"ah...-.. Apa itu..... "
+Zavyre:"ugh... Wanita sialan itu"
+Reina:"Z-ZAVYRE!!!?!!!"
+
+*Zavyre langsung berdiri dengan luka-luka yang cukup parah*
+
+Reina:"Ada apa!!!?!!!"
+Zavyre:"Tidak... Jangan... Aku akan mengalahkannya sendiri, ini urusanku"
+
+*Dari luar kamar Reina mendengar banyak suara langkah kaki yang berlari kearah kamar Reina*
+
+Kryle:"REINA!!!!"
+Kryle mendobrak pintu kamarnya untuk mengecek keadaan Reina.
+
+Kryle:"Kau tidak apa-apa!?Aku mendengar suara hantaman yang sangat keras"
+Reina:"aku baik-baik saja"
+Zavyre:".... Ehem"
+
+*Kryle tersentak ketika melihat Zavyre ada di dalam kamar Reina dan langsung mengangkat senjata nya sembari merangkul Reina sedekat mungkin, begitu juga para prajurit lain yang berdiri di belakang mereka mulai mengangkat senjatanya*
+
+Kryle:"APA YANG KAU LAKUKAN DISINI!!!"
+Zavyre:"Tidak ada waktu untuk konflik diantara kita, Altheas Dia-lah yang seharusnya kalian khawatirkan saat ini"
+Reina:"... Maksudmu?"
+
+Zavyre:".... Dia ingin mengulang peperangan 18 Tahun lalu, atas dasar Dendam"
+
+*Mereka semua terdiam setelah mendengar kata-kata Zavyre, ntah mereka harus percaya dengan apa yang dia katakan atau tidak*
+
+Zavyre:"Kalian mungkin tidak akan mempercayai ku"
+Zavyre melihat kearah Reina
+
+Zavyre:"tapi aku mohon.. Untuk kali ini saja percayalah padaku... Reina"
+
+Langkah kaki yang terburu-buru datang diantara mereka. 
+
+Lioren:"KAPTEN!! PARA PRAJURIT TIMUR TELAH MELIHAT AKTIVITAS SIHIR ASING BEBERAPA MENIT LALU, LAPORAN MEREKA MENGATAKAN SIHIR ITU SANGAT BESAR!"
+
+*Semua orang yang ada disana terkejut mendengarkan kata-kata Lioren*
+
+*Dengan lembut Reina melepaskan diri dari rangkulan Kryle dan mendekati Zavyre*
+
+Kryle:"REINA! JANGAN-..."
+
+*Mata mereka saling bertemu, untuk pertama kalinya salah satu dari mereka tidak memalingkan tatapan mereka masing-masing*
+
+Reina:"Kesempatan mu Hanya datang untuk saat ini saja.. "
+Zavyre hanya menganggukkan sedikit kepalanya
+
+*Reina membalikkan badan ke arah para prajurit dan juga Kryle*
+
+Kryle:"Reina?!..."
+Reina:"KRYLE!!"
+Kryle:"S-Solan!"
+Reina:"Kau dengar apa yang dia katakan?... Ini bukanlah hal yang biasa kita hadapi sebelumnya, Ini adalah pengulangan masa lalu"
+
+Reina:"Mulai Evakuasi Warga kerajaan!"
+*Semua prajurit di sana langsung mematuhi perintah Reina dan langsung pergi meninggalkan mereka bertiga*
+
+Kryle:"Kau mempercayai nya Reina?"
+Reina:"Untuk kali ini saja Kryle"
+Kryle:"......... "
+Kryle:"Aku menunggu Perintah mu selanjutnya Putri"
+
+Reina:"Persiapkan Prajurit di Perbatasan Vulkrath"
+Kryle:"SOLAN!"
+Kryle langsung membalikkan badannya dan hendak meninggalkan ruangan itu dengan Reina dan Zavyre didalamnya. 
+
+*sesaat untuk terakhir kali Kryle menoleh ke arah Reina dan melihat Reina Tersenyum kepada nya, sambil melihat gerakan yang ada pada mulutnya, seakan mengatakan sesuatu yang tidak sempat ia dengar*
+
+Zavyre:"... Reina.. Jika perkiraan ku benar Altheas akan tiba dalam beberapa jam,dia tidak sendirian"
+Reina:"..."
+Zavyre:"yang aku lihat dia membangkitkan penyihir yang sudah mati 18 tahun lalu, mungkin kekuatan mayat penyihir itu tidak terlalu kuat, tapi cukup untuk menjadi sumber sihir untuk Altheas"
+Reina:"....Aku Sudah merasakan sihirnya mendekat "
+Zavyre:"itu dia.. Aku akan menahannya selama yang aku bisa"
+
+*Zavyre hendak meninggalkan Reina juga, tapi sebelum dia sempat pergi.. Reina menarik tangannya*
+
+*Zavyre membalikan badannya dan menatap Reina, sambil meraih tangannya*
+
+Reina:"... Terimakasih"
+Zavyre:"..... Seharusnya aku yang berterimakasih... Reina"
+
+*Zavyre menyentuh wajah Reina sebelum dia benar-benar pergi, dan melepaskan genggaman tangan reina*
+
+Zavyre:"Jaga dirimu Putri Reina"
+Zavyre langsung pergi meninggalkan Reina di kamarnya sendirian. 
+---------------------------------
+_"Sekarang hanya tinggal diriku"_
+
+*Reina menatap langit biru untuuk terakhir kalinya*
+
+_"ayo Reina Demi Vulkrath, Demi semua orang yang kau sayangi"_
+
+_"Celeris"_
+
+*Reina pergi ke ruangan di istana yang memiliki pemancar sihir untuk membuat perisai yang akan melindungi seluruh kerajaan*
+
+_"Scutum Lucis!"_
+
+*Reina membuat sihir perisai ke arah pemancar dan dari pemancar itu sihir perisai langsung menyebar ke seluruh penjuru Vulkrath, dan akhirnya cahaya keemasan menutupi seluruh Vulkrath dengan merata*
+
+*Reina langsung melihat keluar istana*
+
+_"Aku akan melindungi Vulkrath"_
 ---------------------------------`,
 };
 
