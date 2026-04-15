d0f27c5b84c887b89f86e19280a9486781a88331
reinavultador-cloud <reinavultador@gmail.com>
2026-04-15 11:35:46 +0700

    Update chapter 24 details and content

--- src/data/novels/novel2/chapters/ch24.js
+++ src/data/novels/novel2/chapters/ch24.js
@@ -1,10 +1,110 @@
 const chapter = {
-  id: 5,
+  id: 24,
   arc: "Arc 2: Flame Arc",
-  title: "#5 Sumpah Dendam di Balik Luka",
+  title: "#24 Kekuatan yang Saling Menolak",
   content: `
 ---------------------------------
+*Cahaya keemasan dari Scutum Lucis berkilau di seluruh langit Vulkrath. Kubah perisai itu berdenyut pelan seperti jantung raksasa yang melindungi kerajaan.*
 
+*Namun… perisai itu bukanlah akhir. Itu hanyalah awal.*
+
+*Reina berdiri di balkon tertinggi istana, napasnya teratur meski jantungnya berdegup cepat. Rambutnya tertiup angin, sementara matanya menatap hutan di kejauhan tempat suar hitam-merah tadi muncul.*
+
+_"Dia datang...Celeris!"_
+---------------------------------
+*Setelah memastikan perisai cahaya sudah menutupi seluruh bagian kerajaan Reina pergi ke garis depan di perbatasan kerajaan menunggu serangan yang akan muncul kapan saja bersama Kryle*
+
+Kryle:"Reina..."
+Reina:"Ini untuk Vulkrath... Bukan untuk ku,Kryle"
+
+*Kryle mengangguk dan Kryle juga berdiri di garis depan bersama ribuan prajurit kerajaan lainnya. Pedangnya terhunus, tatapannya tajam.*
+
+*Lioren berlari menghampirinya.*
+
+Lioren:"KAPTEN! SIHIR GELAP TERDETEKSI DI UTARA! IA BERGERAK DENGAN CEPAT MENUJU KESINI! "
+
+Kryle menggenggam gagangnya lebih erat.
+Kryle:"Kita sudah siap!"
+
+*Tiba-tiba, langit di utara berubah menjadi merah kehitaman. Awan berputar seperti pusaran kematian*
+
+*Kryle mengangkat pedangnya tinggi-tinggi.*
+
+Kryle:"JANGAN GENTAR! KITA DISINI UNTUK MELINDUNGI KERAJAAN KITA!!"
+
+*Prajurit lainnya bersorak dengan sangat kencang*
+---------------------------------
+*Tiba-tiba benar saja, serangan energi gelap pun terlempar dari jarak yang cukup jauh,di ikuti dengan kemunculan mahluk kegelapan yang jumlahnya tak terhitung mulai menyerang garis depan*
+
+Reina:"Cahaya yang menjaga jiwa dan raga Kami… bangkitlah!- _Aegis Lumen!!_"
+
+*Cahaya berdenyut dari dada Reina, menyebar seperti riak emas ke seluruh medan perang, membuat para prajurit mendapatkan tambahan energi*
+
+Kryle:"PER FIDEM ET GLORIAM!!"
+Prajurit:"SOLAN!"
+*Seluruh Prajurit Maju dengan pimpinan Kryle, Menyerang satu persatu makhluk kegelapan yang muncul*
+
+*Sementara Reina tetap Berdiri di tempat nya dan mulai mencari Keberadaan Sihir Altheas yang tersembunyi oleh sihir makhluk-makhluk gelap*
+
+*Saat sedang mencari kekuatan sihir Altheas Tiba-tiba saja serangan yang cukup besar menghantam medan pertempuran yang membuat para makhluk gelap berkurang dengan cepat*
+
+Zavyre:"ALTHEAS!!!!! KELUAR KAU WANITA KEPARAT!"
+Reina:"KETEMU!!"
+
+*Tiba-tiba tanah bergetar,Bukan getaran biasa.Ini terasa seperti langkah raksasa yang melintasi dunia lain.*
+
+*Langit di utara terbelah.*
+
+*Dari celah hitam pekat itu, muncul lingkaran sihir raksasa berwarna merah darah, berputar perlahan seperti roda takdir yang terkutuk.*
+
+*Dan dari pusat lingkaran itu Altheas turun perlahan*
+
+_Altheas:"Wah... Kau menemukan ku putri Abadi~"_
+
+*Di belakang Altheas melayang mayat-mayat penyihir yang sudah mati di habisi reina 18 tahun lalu.*
+
+*Tubuh mereka pucat, retak-retak seperti porselen pecah, mata mereka kosong namun berpendar merah. Di dada masing-masing terukir segel sihir yang berdenyut seperti jantung.*
+
+*Rantai darah mengikat mereka ke Altheas menjadi sumber kekuatannya.*
+
+*Altheas tersenyum tipis, dingin, dan angkuh.*
+
+Zavyre:"KEPARAT!"
+*Zavyre melontarkan beberapa serangan besar sekaligus, yang membuat beberapa pertahanan Altheas hancur, namun serangan itu juga mengonsumsi banyak sekali energi Zavyre*
+
+*Reina yang menyadari kekuatan Zavyre mengurang dengan sangat cepat, mencoba untuk memberikan sihir energi juga untuk nya namun ternyata, sihir itu malah menolak untuk masuk ke tubuh Zavyre*
+
+Reina:"H.. Hah?!! Bagaimana-"
+
+*Zavyre Terus melemparkan serangan kepada Altheas dengan bertubi-tubi, dan semua serangan nya banyak yang dapat di hindari dan ditangkis oleh kekuatan Altheas*
+
+Altheas:"apa ini Zavyre?, kekuatan mu Berkurang dengan sangat Drastis... Kau sangat lemah"
+Zavyre:"SEBAIKNYA KU TUTUP MULUT MU ITU!!"
+
+Reina:"T-tidak bisa... Apa yang harus ku lakukan jika Dibiarkan Dia bisa kalah"
+
+*tiba-tiba ada makhluk gelap yang menghampiri reina dan hendak menyerangnya*
+
+Reina:"!!!"
+Vyerna:"_Ouroboros Umbra Imperium!"_
+
+*dari dalam tanah yang di lalui oleh makhluk gelap itu, muncul banyak sekali ular dan langsung melilit mereka sampai mati*
+
+Reina:"... Kau-"
+Vyerna:"Kekuatan mu tidak akan masuk Kedalam Tubuh Zavyre Reina, karna energi kegelapan Zavyre menolak energi Cahaya milikmu"
+Reina:"Tapi jika dibiarkan Zavyre"
+Vyerna:"DIA AKAN BAIK-BAIK SAJA!! Lebih baik kau ikut menyerang Altheas, aku akan ada di bawah sini untuk menahan makhluk-makhluk ini"
+
+*Reina mengangguk*
+Reina:"Terimakasih Vyerna! _"CELERIS"_
+*Reina berpindah ke Samping Zavyre, untuk ikut menyerang Altheas yang sudah mulai menggunakan kekuatan dari mayat penyihir*
+
+Altheas:"Akhirnya kau muncul juga putri kecil~_Speculum Ruinae!_"
+
+*Disekeliling Zavyre dan Reina muncul Cermin yang memunculkan serangan balik milik Zavyre*
+
+Zavyre:"REINA!!!"
+Reina:"-!!"
 ---------------------------------`,
 };
 
