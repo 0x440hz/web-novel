b8250acf07b27de8a0ee246653d8701286dd6f3c
reinavultador-cloud <reinavultador@gmail.com>
2026-04-16 19:42:42 +0700

    Add character details for Zavyre

---
+++ src/data/novels/novel2/characters/zavyre.js
@@ -0,0 +1,19 @@
+export default `
+*Biodata Karakter*
+
+Nama:Zavyre 
+Gender:Laki-laki 
+Usia:?? 
+Peran:Antagonis
+Kepribadian:
+  * Dendam dan keras kepala, obsesif terhadap kekalahan masa lalu
+  * Sombong dan meremehkan lawan
+  * Licik, manipulatif, mampu memanfaatkan informasi masa lalu untuk menakut-nakuti musuh
+  * Emosional dan mudah tersulut amarah saat dihadapkan pada kegagalan atau ingatan lama 
+Kemampuan / Kekuatan:
+  * Sihir gelap / penyihir kuat, memiliki energi magis besar
+  * Memiliki kemampuan menyerang jarak jauh dan dekat dengan sihir destruktif
+  * Paham kekuatan Reina dan mencoba memanfaatkannya
+Gaya Bertarung:
+  * Mengandalkan sihir ofensif yang cepat dan kuat`
+  ;
