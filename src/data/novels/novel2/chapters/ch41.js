0d3fc18000f3d65b1649f1773232d7a7a0753eec
reinavultador-cloud <reinavultador@gmail.com>
2026-04-16 19:27:20 +0700

    Create ch41.js

---
+++ src/data/novels/novel2/chapters/ch41.js
@@ -0,0 +1,137 @@
+const chapter = {
+  id: 41,
+  arc: "Arc 2: Flame Arc",
+  title: "#41 Ikatan yang Melampaui Kehancuran",
+  content: `
+---------------------------------
+*Angin berputar liar di sekitar mereka. Lantai batu retak, langit di atas istana runtuh perlahan oleh tekanan dua kekuatan kegelapan yang saling menggigit.*
+
+*Runtuhan lantai istana itu bisa terdengar sampai ke tempat Lioren dan Vyerna yang sudah terbaring di tanah*
+
+Maeryn:"maafkan aku kapten..dan terimakasih sudah mau membimbingku selama aku disini.."
+Lioren:"tch...Kau tahu betul aku adalah orang yang mudah membenci sesuatu..dan sejak tadi aku sudah membencimu"
+> _Lioren mencoba bangkit dari posisinya dibantu oleh tumpuan pada pedangnya dan berdiri dengan lemah namun bisa dipastikan dari matanya seakan ada api yang membara dengan sangat terang_
+
+Lioren:"Aku yang akan mengakhiri dirimu maeryn!.."
+Maeryn:"tch!!.. Umbra-!!"
+> _Tekanan gravitasi di sekitar Maeryn tiba-tiba menjadi berat, dan membuatnya membungkuk sebelum dia selesai merapalkan mantra lainnya, awalnya maeryn mencoba melawan untuk tetap berdiri namun tidak bisa tekanan itu terlalu berat_
+
+Maeryn:"A-APA INI!?!!"
+
+*Lioren telah menempatkan seluruh tekad, kehormatan, dan sumpahnya ke dalam pedangnya,Semua energi di sekitar baik cahaya, kegelapan, bahkan kehampaan tertarik ke bilah pedangnya.*
+
+> _Lioren menggenggam pedangnya dan mulai mengangkatnya, pedangnya bercahaya sangat terang_
+
+Lioren:"_Ultima Regalia! Caelum Ruptor!!!_
+Maeryn:"!!!!"
+*Tidak ada gelombang,Tidak ada ledakan berlebihan,Hanya satu garis cahaya lurus yang membelah ruang,Apa pun yang berada di jalur tebasan itu tidak dihancurkan,melainkan dihapuskan dari kemungkinan untuk bertahan,Perisai tidak bisa menahan,Regenerasi tidak bisa memulihkan, dapat dipastikan apapun yang menghalangi jalur tebasnya akan kehilangan Hak untuk berdiri kembali*
+
+> _Maeryn masih terlihat berdiri dengan tidak menunjukkan tanda apapun_
+Maeryn:".... Heh.. Terimakasih kapten"
+Lioren:"Pergilah.."
+
+*Tubuh Maeryn yang semulanya berdiri dengan tegak tiba-tiba terbelah menjadi dua dan begitu tubuhnya terjatuh ke tanah dia langsung menjadi abu dan tertiup angin cahaya Vulkrath*
+
+Lioren:"...aku sudah terlanjur membencimu, seribu kebaikan yang pernah kau tunjukan padaku telah tertutupi oleh satu kesalahanmu yang sangat fatal seperti ini"
+
+> _Vyerna mulai terbangun dan melihat ke sekelilingnya_
+
+Vyerna:"Lioren..."
+Lioren:"ah!... Vyerna!!Kau baik-baik saja?"
+Vyerna:"Semua mahluk kegelapan itu sudah tidak ada?!"
+> _Lioren menganggukkan kepalanya dan meraih tangan Vyerna_
+
+Lioren:"sudah tidak ada.."
+Vyerna:"Reina... Kita harus kembali padanya"
+Lioren:"kau benar,tapi sebaiknya kau tidak perlu ikut"
+Vyerna:"Aku akan ikut!"
+Lioren:"Tidak!.. Aku akan membawamu ke tempat pengungsian, dan jika boleh ku katakan ini perintah kapten"
+Vyerna:"..... Cih.. Hehe"
+---------------------------------
+*Kembali ke ruang pilar Vulkrath, keadaan sudah sangat sunyi hanya ada suara tawa yang sangat berat dari Astrev*
+
+Astrev:"Hahaha.. Tidak mungkin cangkang kosong tak bernyawa seperti mu bisa mengalahkan ku"
+Zavyre:"Tch.."
+> _Zavyre berdiri kembali namun dia bisa merasakan sebuah tangan menahan kakinya, tangan itu adalah tangan Reina yang menggenggam kaki Zavyre_
+
+Zavyre:"Reina?!.."
+Reina:"Bantu aku berdiri.."
+Astrev:"Heh?.. Apa ini? Putri Vulkrath yang abadi meminta bantuan untuk berdiri dari seorang mayat hidup rendahan?"
+> _Reina dan Zavyre kini berdiri berdampingan dengan tangan mereka saling menggenggam satu sama lain_
+
+*Zavyre merasakan ada kekuatan cahaya yang mengalir ke tangannya dengan jumlah besar dan cepat*
+
+Zavyre:"Reina?!..."
+> _Zavyre melihat kearah reina dengan bingung, namun tangan Reina yang lain langsung meraih pipinya dan membelainya dengan lembut_
+
+Reina:"shhh..kita akan melakukannya bersama.. Seperti dulu.."
+Zavyre:"...bagaimana jika aku tidak bisa!?!"
+Reina:"Kau bisa.. Tubuhmu kini dipenuhi cahaya suci dari diriku"
+Astrev:"heh.. Kalian pikir dengan sihir kecil itu bisa mengalahkan ku?! Pertarungan sesungguhnya baru akan dimulai!!"
+
+Astrev:_"Aperire Sigillum Tenebrae!!!"_
+
+*Tubuhnya terangkat dari tanah. Jubahnya terurai menjadi asap hitam. Kulitnya mengelupas seperti abu yang terbakar, memperlihatkan wujud aslinya.*
+
+> _Reina menggenggam lebih erat tangan Zavyre, begitupun Zavyre yang mulai melangkah sedikit kedepan Reina_
+
+Zavyre:"Ini sihir terakhirnya Reina..aku akan membuatkan Perisai terlebih dulu.._"Tenebris Bastion!"_
+
+*Perisai buatan Zavyre mengelilingi mereka berdua, mereka bisa melihat tubuh Astrev mulai berubah perlahan-lahan*
+
+*Retakan hitam menjalar dari dadanya, seperti kaca yang pecah dari dalam. Cahaya merah tua menyembur dari celah-celah kulitnya.*
+
+*Astrev tertawa pelan. Lalu suaranya berubah menjadi berat, bergema, seolah dua suara berbicara bersamaan.*
+
+Astrev:_"Aperire Sigillum Tenebrae."_
+
+*Tubuh Astrev terangkat dari tanah. Jubahnya terurai menjadi asap hitam. Kulitnya mengelupas seperti abu yang terbakar, memperlihatkan wujud aslinya.*
+
+*Tinggi tubuhnya kini hampir dua kali lipat manusia biasa,Kulitnya bukan lagi kulit melainkan lapisan bayangan yang terus bergerak seperti cairan pekat.*
+
+*Dari punggungnya tumbuh enam sayap patah, bukan berbulu melainkan bilah-bilah tulang hitam yang diselimuti kabut gelap.*
+
+*Wajahnya setengah masih menyerupai manusia, namun sisi lainnya terbuka memperlihatkan rongga kosong dengan cahaya merah menyala di dalamnya. Tidak ada mata hanya dua pusaran energi hitam yang berputar perlahan.*
+
+*Tanduk melengkung tumbuh dari pelipisnya, retak dan berdenyut seperti batu magma,Tangan kanannya berubah menjadi cakar panjang dengan urat-urat cahaya merah yang mengalir seperti lava.*
+
+*Setiap langkahnya meninggalkan bekas hangus di lantai batu.*
+
+Reina:"Akhinya dia mengeluarkan seluruh kekuatannya"
+Zavyre:"benar, kita juga akan menghancurkannya dengan seluruh kekuatan terbesar kita"
+
+> _reina mengangguk_
+
+Zavyre:"Tuntun Jalannya Reina.. "
+
+Astrev:"Jangan terlalu percaya diri!! _Abyssal Dominion"_
+
+*Langit di atas mereka berubah hitam sepenuhnya. Lingkaran sihir raksasa muncul, menurunkan hujan rantai bayangan yang menghantam tanah seperti meteor.*
+
+*Namun Serangan itu ditahan oleh perisai buatan Zavyre*
+
+Reina:"Wujud mu itu tidak akan membuat kita mundur Astrev!"
+Astrev:"Dasar semut Cerewet!!!"
+Zavyre:"Bersiaplah Reina ini serangannya yang terakhir"
+Reina:"Baik!"
+
+Astrev:_"Nihil Deus! Final Catastrophe"_
+> _Astrev membuka keenam sayap tulangnya,Tubuhnya terangkat, dan seluruh energi di sekitar,baik cahaya maupun kegelapan,ditarik paksa ke dalam dirinya,Lalu ia menghantamkan cakar ke tanah,Bukan ledakan biasa,Melainkan runtuhnya realitas di area tersebut, ruang menjadi terlipat dan hancur seperti kain yang disobek._
+
+*Saat serangan itu mengarah ke Reina dan Zavyre, Reina langsung memperkuat lagi Perisai buatan Zavyre*
+
+*Serangan Astrev lagi-lagi tidak meretakkan sedikitpun perisai mereka berdua*
+
+Reina:"Kau Sudah tamat Astrev.."
+Zavyre:"Kita akan mengakhiri dirimu disini"
+
+> _wajah astrev langsung membuat raut wajah marah yang seakan tidak terima_
+
+Astrev:"Tidak sebelum aku menghabisi kalian!!!"
+
+*Zavyre&Reina*
+_"Lux in Tenebrae… Concordia Absoluta!!"_
+---------------------------------`,
+};
+
+export default chapter;
