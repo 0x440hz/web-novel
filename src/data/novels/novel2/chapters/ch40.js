de8eba15a17ab50ba9568e63ea31cdbbd1c1e6b3
reinavultador-cloud <reinavultador@gmail.com>
2026-04-16 19:24:25 +0700

    Update ch40.js

--- src/data/novels/novel2/chapters/ch40.js
+++ src/data/novels/novel2/chapters/ch40.js
@@ -1,10 +1,66 @@
 const chapter = {
-  id: 31,
+  id: 40,
   arc: "Arc 2: Flame Arc",
-  title: "#31 Kenangan yang Tak Siap Dilepaskan",
+  title: "#40 Bangkit Demi Satu Nama",
   content: `
 ---------------------------------
+*Reina Melemah, melihat ke arah Zavyre yang mulai menghilang..*
+*Dengan sekuat tenaga Reina mencoba untuk menghampiri tubuh Zavyre sambil mengulurkan tangannya*
+*Setelah ia cukup dekat dengan tubuh Zavyre, ia pun mencoba meraih tangan Zavyre dengan tangannya dengan tanda sihir yang di berikan Zavyre..*
 
+Astrev:"Teruslah mencoba Putri.._Tenebrae Siphon!_"
+Reina:"Agh.... Nhn.."
+
+*Sebuah sihir gelap mulai menyerap kekuatan Reina Dengan cukup cepat sehingga membuat sedikit perubahan pada penampilan Reina, Reina menjadi pucat seiring Kekuatan nya di serap oleh Astrev*
+
+Reina:"...Zavyre.. Buka matamu!.. Raih tanganku!! Aku tidak akan membiarkanmu Hilang dari Hidupku untuk kedua kalinya!"
+Astrev:"Heh.. Omong kosong,aku akan mengambil semua yang kau punya!!"
+Reina:".... Nghhh.. Zavyre"
+
+*Kesadaran Reina mulai menghilang, tapi disaat yang sama Reina merasakan tangan yang hangat meraihnya dan dengan sekejap terjadi dorongan sihir yang kuat sehingga Mantra Astrev Terhenti*
+
+Astrev:"ugh!?!.. A.. Apa yang-"
+Zavyre:"Kau memang cocok di sebut penghianat kerajaan kita astrev!"
+
+*dengan sekejap Zavyre sudah berada diatas Astrev Dan langsung menendang Kepalanya ke tanah*
+
+*Debu dan pecahan tanah beterbangan saat tubuh Astrev menghantam lantai. sihir yang menyerap kekuatan Reina Terputus dan Sihir Reina mulai kembali ke dalam tubuhnya perlahan-lahan*
+
+*Zavyre Berdiri di belakang Reina untuk berhadapan langsung dengan Astrev*
+
+Zavyre:"Reina Terimakasih untuk tidak pernah menyerah padaku.. Kali ini… aku yang akan menghadapi orang ini."
+
+*Astrev perlahan bangkit, darah mengalir dari sudut bibirnya. Senyumnya terangkat dingin.*
+
+Astrev:"Heh… bangkit dari kematian hanya untuk mati lagi? Sangat Menyedihkan, Zavyre!..hanya dengan Ingatanmu kembali tidak akan membuatku mundur!"
+
+*Zavyre mengepalkan tangannya. Aura gelap bercampur cahaya merah tua mulai menyelimuti tubuhnya, berdenyut seperti jantung.*
+
+Zavyre:"Lihat saja Pengkhianat, tubuh kosong tanpa jiwa ini akan mengalahkan dirimu.."
+
+*Astrev mengangkat tangannya, puluhan lingkaran sihir hitam muncul di udara.*
+Astrev:"Kalau begitu, mari kita lihat… siapa yang akan dikosongkan lebih dulu!!."
+Astrev:"_Umbra Lance!!_"
+*Puluhan tombak bayangan melesat menuju Zavyre.*
+
+*Zavyre melangkah maju tanpa ragu.*
+Zavyre:_"Void Aegis!"_
+
+*Perisai kegelapan tebal muncul di depan tubuhnya. Tombak bayangan menghantam dan hancur menjadi debu hitam.*
+
+*Dan dalam sekejap tubuh Zavyre menghilang dari tempatnya.*
+
+Astrev:"!!?!-"
+Zavyre:"Gradus Umbrae!"
+*Ia muncul tepat di depan Astrev, dengan tinjunya yang dilapisi energi kehampaan.*
+
+*Pukulan itu menghantam perut Astrev dan melemparkannya jauh ke belakang.*
+
+Astrev:"tch....kalian sangat merepotkan..mengapa kau begitu mati-matian untuk membantu putri itu!?!"
+*Zavyre menatapnya dengan mata yang berpendar gelap.*
+Zavyre:"Aku rela mengorbankan segalanya… demi dia yang tidak menyerah padaku..walaupun itu berarti akan merenggut nyawaku"
+
+*Dua kekuatan kegelapan saling bertabrakan, dan takdir mereka mulai ditentukan dalam badai kehampaan.*
 ---------------------------------`,
 };
 
