7e4b2d4651e65c664cfe8537eda444e8d7107a31
reinavultador-cloud <reinavultador@gmail.com>
2026-04-16 19:23:20 +0700

    Update ch39.js

--- src/data/novels/novel2/chapters/ch39.js
+++ src/data/novels/novel2/chapters/ch39.js
@@ -1,10 +1,130 @@
 const chapter = {
-  id: 31,
+  id: 39,
   arc: "Arc 2: Flame Arc",
-  title: "#31 Kenangan yang Tak Siap Dilepaskan",
+  title: "#39 Tangisan Cahaya yang Terbungkam",
   content: `
 ---------------------------------
+*Di desa keadaan masih sangat kacau,Semua penduduk yang selamat berhasil dibawa te tempat yang lebih aman, Vyerna dan Lioren sudah mulai kelelahan menghadapi makhluk-makhluk kegelapan yang terus menerus muncul*
 
+Vyerna:"..Ah..bagaimana Reina dan Kaelous"
+
+*Lioren mengayunkan pedangnya sekali lagi*
+Lioren:"hah... Aku tidak tahu.. Tapi untuk sesaat sebelumnya.. Aku sempat merasakan ada salah satu sihir yang menghilang"
+Vyerna:"Aku juga merasakannya.."
+Lioren:"Aku harap bukan hal buruk... "
+
+*Vyerna melihat kearah ruang pilar sihir istana Vulkrath, dan dia melihat sudah banyak lingkaran yang terbuat dari benang cahaya dan juga bintang-bintang yang sedang menyebar ke seluruh langit sekitar Vulkrath*
+
+Vyerna:".. L-lioren?... "
+Lioren:"..ini tidak bagus..apa yang terjadi pada Reina!?-"
+*tiba-tiba makhluk kegelapan kembali menyerang mereka dengan hendak menerkam Lioren*
+
+Vyerna:"LIOREN!!!"
+Lioren:"Preservant!!..."
+*Sebuah kubah perisai mengelilingi mereka berdua dan membuat mahluk tadi langsung hancur, begitupun perisai tersebut*
+
+Vyerna:"Kau tidak apa-apa?!"
+*Lioren mengangguk*
+
+*tiba-tiba Lioren berdiri di depan Vyerna sambil memegang pedangnya kedepan*
+
+Lioren:"Kebelakang.. Ada yang datang"
+Vyerna:"....-"
+
+*Dari balik debu reruntuhan, ternyata Maeryn yang muncul disitu*
+
+Lioren:"MAERYN!!?!!, Ternyata kau..."
+Vyerna:"MAERYN!!.. Kau baik-baik saja?"
+*Maeryn hanya menunduk dengan senyap,dan Vyerna hendak menghampiri maeryn lebih dekat untuk memastikan dia baik-baik saja*
+
+Vyerna:"Maeryn...Kau..-"
+Maeryn:_"Atra Invocatio"_
+Vyerna:"-!!"
+Lioren:"VYERNA!!!!"
+
+*Tiba-tiba ada dua mahluk Kegelapan berbentuk seperti Ular dengan sisik tajam yang menyerang Vyerna dari kedua sisi*
+
+*Dua makhluk kegelapan berbentuk ular itu melesat dari lingkaran sihir Maeryn, sisik mereka berkilau seperti baja hitam, rahang terbuka lebar.*
+*Lioren bergerak tanpa berpikir. Ia melompat ke depan Vyerna, mengayunkan pedangnya.*
+
+Lioren:"Jangan sentuh dia!!"
+
+*Bilahnya menebas salah satu ular itu, memaksanya terhuyung ke samping. Namun yang lain berhasil menyelinap, melingkari kaki Vyerna.*
+
+Vyerna:"Hah!!!!-.._Serpens Oblivio!_"
+Lioren:"Maeryn .. Apa yang kau lakukan!!!?!"
+Maeryn:".... Mungkin kalian sudah tau apa yang ku lakukan.."
+Vyerna:"...kau bekerja sama dengan penyihir itu kan!?!"
+Lioren:"SIALAN!! AKU AKAN MENGHANCURKAN MU JUGA SEPERTI MAKHLUK-MAKHLUK SIALAN PANGGILAN MU ITU,MAERYN!!!"
+
+Maeryn:_"Umbra Nexus"_
+Lioren:_"Gladius Iuramentum!!!!!"_
+Vyerna:_"Viperis Noctis! "_
+---------------------------------
+*Kembali ke Ruang pilar Reina sedang merapalkan mantra tanpa henti dengan di kelilingi oleh perisai cahaya yang sangat kuat, sementara itu Astrev dan Zavyre terus mencoba memecahkan perisai yang melindungi Reina*
+
+Zavyre:"... APA YANG DIA LAKUKAN!?!"
+Astrev:"Sial.. Tidak ada serangan kita satu pun yang berhasil.."
+Zavyre:"Tch,wanita yang menyebalkan, _"TENEBROUSE!"_
+
+*Zavyre terus-menerus melontarkan serangan sekuat tenaga sedangkan Astrev terus memikirkan apa yang harus dilakukan*
+
+Astrev:"apa... Apa yang bisa melemahkan putri itu-......"
+
+*Tiba-tiba pikirannya memutar kembali percakapan mereka dengan Maeryn di reruntuhan*
+
+(Percakapan di reruntuhan) 
+_Maeryn:"sampai saat ini yang kutahu salah satu hal yang bisa membuat nya goyah adalah emosinya sendiri"_
+
+_Astrev:"Emosinya ya?"_
+
+_Maeryn:"Dan yang paling bisa membuat pertahanannya goyah adalah keterlibatan dia dengan orang yang dia sayangi, seperti Teman-teman nya itu"_
+
+_Zavyre:"Hah.. Itu mudah untuk dilakukan, kita hanya perlu melenyapkan salah satu dari temannya itu"_
+---------------------------------
+*Astrev langsung tersadar dari lamunan-nya dan langsung terpikirkan satu hal yang bisa melemahkan Reina*
+
+Astrev:"heh... Aku tahu sekarang"
+_"Nexus Bestiae! "_
+
+*Zavyre yang sedang menyerang perisai Reina tiba-tiba terhenti dengan paksa, dan muncul rantai tajam yang melilit diri-Nya*
+
+Zavyre:" Ha!?!!!,APA INI!!"
+*Zavyre langsung melihat kearah astrev yang rupanya membuat mantra pengikat untuk Zavyre*
+
+Zavyre:"ASTREV!! APA YANG KAU LAKUKAN DASAR KEPARAT!BUKAN KAH KAU INGIN MENANG,MENGAPA KAU MENGHENTIKAN KU!?!"
+
+Astrev:"... Heh.. Tentu saja aku ingin menang.. Tapi.."
+
+*Astrev Mengangkat tubuh Zavyre ke udara dengan mengendalikan rantainya*
+Zavyre:"APA YANG KAU-!!?!"
+
+Astrev:"aku ingin menang tapi....tidak bersama dengan mu lagi..._"Lancea Tenebris!"_
+
+*Muncul lima Tombak yang muncul dari bawah Zavyre dan langsung menusuknya dengan mudah*
+
+Astrev:"Kau sangat payah Zavyre, aku baru teringat kembali jika kau hanyalah jiwa mati yang dimanfaatkan olehku"
+Zavyre:"B-Ba.. Ji.. Ngan"
+Astrev:"Senang bisa bertemu dengan mu lagi adikku.. Sayang sekali harus berakhir disini:"
+
+*Tiba-tiba astrev merasakan aura sihir cahaya Reina mulai melemah*
+
+Astrev:"heh.. Aku berhasil"
+
+*Reina dengan Cepat tiba-tiba langsung menyerang nya dengan serangan yang tidak berhenti namun presisi dan kecepatan nya jauh lebih lemah dari sebelumnya*
+
+Reina:"MATILAH KAU ASTREV!!!!!"
+Astrev:"Heh... Dasar Lemah"
+_"Regnum Tenebrae"_
+Reina:"!!!... "
+*Astrev Menari Reina kedalam ruang kegelapan abadi dan mengikatnya dengan rantai yang sama dengan Zavyre sebelumnya*
+Astrev:_"Vinculum Ruinae!!!!!"_
+
+*Sebelum melakukan hal lain Rantai yang mengikat Reina meledak disekelilingnya dan bisa di pastikan karna hal itu Reina kehilangan kesadarannya dengan cepat*
+
+*Astrev langsung menjatuhkan tubuh Reina ke tanah dekat dengan tubuh Zavyre yang mulai menjadi abu karena kehilangan Sihir, begitu juga Ruangan gelap yang astrev buat langsung sirna*
+
+Astrev:".. Heh.. Dengan ini aku akan merenggut semuanya dari-Mu Dan menetapkan runtuhnya kekuasaan mu, Putri Vulkrath.."
 ---------------------------------`,
 };
 
