748c7ac197bdb058f6eed9d1c50d52d63092dea5
reinavultador-cloud <reinavultador@gmail.com>
2026-04-19 05:12:18 +0700

    Update vyerna.js

--- src/data/novels/novel2/characters/vyerna.js
+++ src/data/novels/novel2/characters/vyerna.js
@@ -1,1 +1,13 @@
+export default `
+*Biodata Karakter*
 
+Nama: Vyerna
+Gender: Perempuan
+Usia: 126 tahun
+Peran: Pengamat manipulatif / penggoda / pemantik konflik batin Zavyre
+Kepribadian:Jahil, licik, penuh intuisi, provokatif, senang memainkan emosi orang lain
+Kemampuan / Kekuatan: Peka terhadap emosi dan perasaan tersembunyi, hipnosis halus melalui suara dan tatapan, sihir racun serta ilusi, mampu membaca keraguan lawanGaya Bertarung: Tidak frontal; mengandalkan manipulasi mental, sihir jarak menengah, ilusi, dan serangan racun yang melemahkan musuh perlahan
+Hubungan dengan Karakter Lain:** Terikat dengan Zavyre—hubungan ambigu antara ketertarikan, ejekan, dan permainan psikologis
+Ciri khas: Desisan “psss~”, tatapan mata ular yang tajam, gerakan melingkar dan mendekat tanpa suara
+Motivasi: Menikmati konflik batin Zavyre, mengungkap perasaan yang disembunyikan, serta menjaga kesenangan dan keseimbangan versinya sendiri di tengah kekacauan`
+  ;
