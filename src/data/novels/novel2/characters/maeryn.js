dda68aae7a202674a2b7808ebb8e40e82410dd11
reinavultador-cloud <reinavultador@gmail.com>
2026-04-19 15:26:12 +0700

    Update character Maeryn's biodata

--- src/data/novels/novel2/characters/maeryn.js
+++ src/data/novels/novel2/characters/maeryn.js
@@ -1,3 +1,13 @@
 export default `
-placeholder doang ini
-`;
+*Biodata Karakter*
+
+Nama: Maeryn
+Gender:Perempuan
+Usia:23 tahun
+*Peran:Prajurit Divisi Keamanan Dalam Istana
+*Kepribadian:Tenang, sopan, waspada, bertanggung jawab, terlihat lembut tapi berani.
+*Gaya Bertarung:Defensi
+*Ciri Khas:Gerakan senyap, mata Biru pucat, insting bahaya kuat.
+*Motivasi:Membuktikan diri dan melindungi.
+`
+;
