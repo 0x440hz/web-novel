efe7f798a782b6cf863de22ee93b334ed5b0566c
reinavultador-cloud <reinavultador@gmail.com>
2026-04-19 15:27:47 +0700

    Add biodata for character Kaelous
    Added character biodata for Kaelous, including name, title, age, race, status, fighting style, and personality traits.

--- src/data/novels/novel2/characters/kaelous.js
+++ src/data/novels/novel2/characters/kaelous.js
@@ -1,3 +1,13 @@
 export default `
-placeholder doang inj
-`;
+*Biodata Karakter*
+
+Nama: Kaelous
+Julukan:The Timeless One
+Usia:70+
+Ras: Manusia
+Status: Pengembara lintas waktu
+*Gaya bertarung: Penghentian Waktu,dan pengembalian waktu dengan syarat. 
+*Kepribadian:
+Tenang, lembut, penuh empati. Jarang menunjukkan emosi, tetapi sangat protektif terhadap Reina. Menyimpan banyak rahasia dan sering berbicara dengan makna tersirat.
+`
+;
