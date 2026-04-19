71c3a56f0e228d95b97e837677b5d4cb94297ac9
reinavultador-cloud <reinavultador@gmail.com>
2026-04-19 15:24:28 +0700

    Update fighting style description for Lioren

--- src/data/novels/novel2/characters/lioren.js
+++ src/data/novels/novel2/characters/lioren.js
@@ -7,7 +7,7 @@ Usia:21 tahun
 *Peran: Prajurit Kerajaan-Divisi Keamanan Wilayah Barat
 *Kepribadian: Ceria, antusias, ingin tahu, setia, bermartabat, menghormati atasan. 
 *Kemampuan / Kekuatan: Disiplin tempur baik, peka terhadap situasi sekitar, loyal dan mental stabil
-*Gaya Bertarung: Frontal dan terstruktur dengan pedang; mengutamakan pertahanan dan kerja tti
+*Gaya Bertarung: Frontal dan terstruktur dengan pedang; mengutamakan pertahanan dan kekuatan
 *Ciri Khas: suara lantang, ekspresi jujur
 *Motivasi: Mengabdi pada kerajaan dan menjaga kehormatan istana
 `
