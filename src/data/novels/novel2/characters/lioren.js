f715879e782d2269473ff9d8118f0c9d1a906e7c
reinavultador-cloud <reinavultador@gmail.com>
2026-04-19 15:22:26 +0700

    Update lioren.js

--- src/data/novels/novel2/characters/lioren.js
+++ src/data/novels/novel2/characters/lioren.js
@@ -1,4 +1,13 @@
 export default `
-placeholder doang ini
-`;
+*Biodata Karakter*
 
+Nama:Lioren
+Gender:Laki-laki
+Usia:21 tahun
+*Peran: Prajurit Kerajaan-Divisi Keamanan Wilayah Barat
+*Kepribadian: Ceria, antusias, ingin tahu, setia, bermartabat, menghormati atasan. 
+*Kemampuan / Kekuatan: Disiplin tempur baik, peka terhadap situasi sekitar, loyal dan mental stabil
+*Gaya Bertarung: Frontal dan terstruktur dengan pedang; mengutamakan pertahanan dan kerja tti
+*Ciri Khas: suara lantang, ekspresi jujur
+*Motivasi: Mengabdi pada kerajaan dan menjaga kehormatan istana
+`;
